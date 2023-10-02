246c8782e44247fc89d681cfcacd4ffb3a0872e3
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-02 10:56:34 -0600

    refactor(src/attribute/StringAttribute): extend Attribute class

--- src/attribute/StringAttribute.ts
+++ src/attribute/StringAttribute.ts
@@ -1,35 +1,12 @@
-import type { AttributeValue } from '@aws-sdk/client-dynamodb';
-import { z } from 'zod';
-import { InvalidAttributeNameError } from './error/index.js';
+import { AttributeValue } from '@aws-sdk/client-dynamodb';
+import { Attribute } from './Attribute.js';
 
-export const AttributeNameSchema = z
-  .string()
-  .min(1)
-  .max(255)
-  .regex(/^[a-zA-Z0-9._-]+$/);
-
-export class StringAttribute {
-  public constructor(
-    public readonly name: string,
-    public readonly value: string,
-  ) {
-    const nameParsingResult = AttributeNameSchema.safeParse(name);
-    if (!nameParsingResult.success) {
-      throw new InvalidAttributeNameError(name);
-    }
-
-    this.name = name;
+export class StringAttribute extends Attribute<string> {
+  public constructor(name: string, value: string) {
+    super(name, value);
   }
 
-  public get dynamoDbValue(): AttributeValue {
+  public override get dynamoDbValue(): AttributeValue {
     return { S: this.value };
-  }
-
-  public get namePlaceholder(): string {
-    return `#${this.name}`;
-  }
-
-  public get valuePlaceholder(): string {
-    return `:${this.name}`;
   }
 }
