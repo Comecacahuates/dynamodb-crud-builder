b978aea27da078e1cdd1820e749e32892fda6413
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-02 13:29:10 -0600

    update(attribute/Attribute): custom validation

--- src/attribute/Attribute.ts
+++ src/attribute/Attribute.ts
@@ -1,6 +1,9 @@
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { z } from 'zod';
-import { InvalidAttributeNameError } from './error/index.js';
+import {
+  InvalidAttributeNameError,
+  InvalidAttributeValueError,
+} from './error/index.js';
 
 export const AttributeNameSchema = z
   .string()
@@ -8,13 +11,21 @@ export const AttributeNameSchema = z
   .max(255)
   .regex(/^[a-zA-Z0-9._-]+$/);
 
+export interface AttributeOptions {
+  validationSchema: z.ZodType;
+}
+
 export abstract class Attribute<V = unknown> {
   private internalValue: V;
+  private validationSchema: z.ZodType;
 
   public constructor(
     public readonly name: string,
     value: V,
+    options: AttributeOptions,
   ) {
+    this.validationSchema = options.validationSchema;
+
     const nameParsingResult = AttributeNameSchema.safeParse(name);
     if (!nameParsingResult.success) {
       throw new InvalidAttributeNameError(name);
@@ -22,7 +33,7 @@ export abstract class Attribute<V = unknown> {
 
     this.name = name;
 
-    this.internalValue = value;
+    this.internalValue = this.parseValue(value);
   }
 
   public get value(): V {
@@ -37,5 +48,14 @@ export abstract class Attribute<V = unknown> {
 
   public get valuePlaceholder(): string {
     return `:${this.name}`;
+  }
+
+  protected parseValue(value: unknown): V {
+    const parsingResult = this.validationSchema.safeParse(value);
+    if (!parsingResult.success) {
+      throw new InvalidAttributeValueError(this.name, parsingResult.error);
+    }
+
+    return parsingResult.data;
   }
 }
--- src/attribute/StringAttribute.ts
+++ src/attribute/StringAttribute.ts
@@ -1,9 +1,19 @@
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
+import { z, ZodString } from 'zod';
 import { Attribute } from './Attribute.js';
 
 export class StringAttribute extends Attribute<string> {
-  public constructor(name: string, value: string) {
-    super(name, value);
+  public constructor(
+    name: string,
+    value: string,
+    options?: { validationSchema?: ZodString },
+  ) {
+    const defaultValidationSchema = z.string();
+
+    super(name, value, {
+      validationSchema: defaultValidationSchema,
+      ...options,
+    });
   }
 
   public override get dynamoDbValue(): AttributeValue {
