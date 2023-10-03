85073e03f6a4b420f8d492016e362c1107285812
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 15:07:45 -0600

    test(attribute/Attribute): remove validations

--- test/attribute/StringAttribute.test.ts
+++ test/attribute/StringAttribute.test.ts
@@ -1,9 +1,8 @@
 import { describe, it, expect } from '@jest/globals';
-import { z } from 'zod';
 import { StringAttribute } from '../../src/attribute/StringAttribute.js';
 import {
   InvalidAttributeNameError,
-  InvalidAttributeValueError,
+  InvalidAttributeTypeError,
 } from '../../src/attribute/error/index.js';
 
 describe('Creating string attribute', () => {
@@ -64,73 +63,6 @@ describe('Creating string attribute', () => {
   });
 });
 
-describe('Value validation', () => {
-  describe('Default validation schema', () => {
-    it('should now throw error if value is a string', () => {
-      expect(() => new StringAttribute('attribute-name', 'a')).not.toThrow();
-    });
-
-    it.each([[undefined], [null], [false], [1]])(
-      `should throw error if value is %s`,
-      (value: any) => {
-        expect(
-          () => new StringAttribute('attribute-name', value as any),
-        ).toThrow(InvalidAttributeValueError);
-      },
-    );
-  });
-
-  describe('Custom validation schema', () => {
-    describe.each([
-      [
-        'non empty string',
-        z.string().min(1),
-        'a',
-        [[undefined], [null], [false], [1], ['']],
-      ],
-      [
-        'an email',
-        z.string().email(),
-        'email@email.com',
-        [[undefined], [null], [false], [1], ['not-an-email']],
-      ],
-      [
-        'a url',
-        z.string().url(),
-        'https://www.url.com',
-        [[undefined], [null], [false], [1], ['not-a-url']],
-      ],
-    ])(
-      `Validate value is %s`,
-      (
-        _,
-        validationSchema: z.ZodString,
-        validValue: string,
-        invalidValues: any[],
-      ) => {
-        it(`should not throw error if value is ${validValue}`, () => {
-          expect(() => {
-            new StringAttribute('attribute-name', validValue, {
-              validationSchema,
-            });
-          }).not.toThrow();
-        });
-
-        it.each(invalidValues)(
-          `should throw error if value is %s`,
-          (invalidValue) => {
-            expect(() => {
-              new StringAttribute('attribute-name', invalidValue, {
-                validationSchema,
-              });
-            }).toThrow(InvalidAttributeValueError);
-          },
-        );
-      },
-    );
-  });
-});
-
 describe('Setting value', () => {
   describe('Default validation schema', () => {
     it('should set a new value', () => {
@@ -142,81 +74,36 @@ describe('Setting value', () => {
 
       expect(stringAttribute.value).toBe('new-value');
     });
+  });
+});
 
-    it.each([[undefined], [null], [false], [1]])(
-      `should throw error if value is %s`,
-      (value: any) => {
-        const stringAttribute = new StringAttribute(
-          'attribute-name',
-          'attribute-value',
-        );
+describe('Parsing DynamoDB value', () => {
+  it('should parse value', () => {
+    const stringAttribute = StringAttribute.parse('attribute-name', {
+      'attribute-name': { S: 'attribute-value' },
+    });
 
-        expect(() => stringAttribute.setValue(value)).toThrow(
-          InvalidAttributeValueError,
-        );
-      },
-    );
+    expect(stringAttribute).toBeDefined();
+    expect(stringAttribute!.value).toBe('attribute-value');
   });
 
-  describe('Custom validation schema', () => {
-    describe.each([
-      [
-        'non empty string',
-        z.string().min(1),
-        'a',
-        'b',
-        [[undefined], [null], [false], [1], ['']],
-      ],
-      [
-        'an email',
-        z.string().email(),
-        'email@email.com',
-        'new-email@email.com',
-        [[undefined], [null], [false], [1], ['not-an-email']],
-      ],
-      [
-        'a url',
-        z.string().url(),
-        'https://www.url.com',
-        'https://www.new.url.com',
-        [[undefined], [null], [false], [1], ['not-a-url']],
-      ],
-    ])(
-      `Validate value is %s`,
-      (
-        _,
-        validationSchema: z.ZodString,
-        originalValue: string,
-        newValidValue: string,
-        newInvalidValues: any[],
-      ) => {
-        it(`should not throw error if value is ${newValidValue}`, () => {
-          const stringAttribute = new StringAttribute(
-            'attribute-name',
-            originalValue,
-            { validationSchema },
-          );
+  it('should return undefined if value is not present', () => {
+    const stringAttribute = StringAttribute.parse('attribute-name', {});
 
-          expect(() => {
-            stringAttribute.setValue(newValidValue);
-          }).not.toThrow();
-        });
+    expect(stringAttribute).toBeUndefined();
+  });
 
-        it.each(newInvalidValues)(
-          `should throw error if value is %s`,
-          (newInvalidValue: any) => {
-            const stringAttribute = new StringAttribute(
-              'attribute-name',
-              originalValue,
-              { validationSchema },
-            );
+  it('should throw error if attribute descriptor is not S', () => {
+    expect(() =>
+      StringAttribute.parse('attribute-name', { 'attribute-name': { N: '1' } }),
+    ).toThrow(InvalidAttributeTypeError);
+  });
 
-            expect(() => {
-              stringAttribute.setValue(newInvalidValue as any);
-            }).toThrow(InvalidAttributeValueError);
-          },
-        );
-      },
-    );
+  it('should throw error if dynamodb attribute value is not string', () => {
+    expect(() =>
+      StringAttribute.parse('attribute-name', {
+        'attribute-name': { S: 1 },
+      } as any),
+    ).toThrow(InvalidAttributeTypeError);
   });
 });
