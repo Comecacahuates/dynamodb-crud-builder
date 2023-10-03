ed651839c13efb3eeaa952137d199cdc8f981a6b
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 16:36:10 -0600

    update(attribute/DateAttribute): parse method

--- src/attribute/DateAttribute.ts
+++ src/attribute/DateAttribute.ts
@@ -1,5 +1,8 @@
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
+import { match, P } from 'ts-pattern';
+import { z } from 'zod';
 import { Attribute } from './Attribute.js';
+import { InvalidAttributeTypeError } from './error/index.js';
 
 export class DateAttribute extends Attribute<Date> {
   public constructor(name: string, value: Date) {
@@ -8,5 +11,36 @@ export class DateAttribute extends Attribute<Date> {
 
   public override get dynamoDbValue(): AttributeValue {
     return { S: this.value.toISOString() };
+  }
+
+  public static parse(
+    attributeName: string,
+    dynamodbItem: Record<string, AttributeValue>,
+  ): DateAttribute | undefined {
+    return match(dynamodbItem)
+      .with(
+        {
+          [attributeName]: {
+            S: P.select(
+              P.when((value) => {
+                const dateValidationSchema = z.string().datetime();
+                const dateParsingResult = dateValidationSchema.safeParse(value);
+                const isValidDate = dateParsingResult.success;
+                return isValidDate;
+              }),
+            ),
+          },
+        },
+        (value) => new DateAttribute(attributeName, new Date(value!)),
+      )
+      .with({ [attributeName]: P.select() }, (dynamodbValue) => {
+        throw new InvalidAttributeTypeError(
+          attributeName,
+          'S',
+          'string',
+          dynamodbValue,
+        );
+      })
+      .otherwise(() => undefined);
   }
 }
