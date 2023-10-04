92102c1fdc7f1d57ce0f7c3c888729d502401b31
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 20:55:03 -0600

    test(schema/Schema): attributes

--- test/__mocks__/MockSchema.ts
+++ test/__mocks__/MockSchema.ts
@@ -1,58 +1,32 @@
-import { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { Schema } from '../../src/schema/Schema.js';
-import { Attribute, StringAttribute } from '../../src/attribute/index.js';
 
 export class MockSchema extends Schema {
-  public readonly attribute0: StringAttribute;
-  public readonly attribute1: StringAttribute;
+  public readonly attribute0: string;
+  public readonly attribute1: string;
 
   public constructor(attribute0: string, attribute1: string) {
     super();
-    this.attribute0 = new StringAttribute('attribute-name-0', attribute0);
-    this.attribute1 = new StringAttribute('attribute-name-1', attribute1);
+    this.attribute0 = attribute0;
+    this.attribute1 = attribute1;
   }
 
-  public override getEntityNameValue(): string {
+  public override get entityName(): string {
     return 'MockSchema';
   }
 
-  public override getPkValue(): string {
-    return `PARTITION_KEY#${this.attribute0.value}`;
+  public override get pk(): string {
+    return `PARTITION_KEY#${this.attribute0}`;
   }
 
-  public override getSkValue(): string {
-    return `SORT_KEY#${this.attribute1.value}`;
+  public override get sk(): string {
+    return `SORT_KEY#${this.attribute1}`;
   }
 
-  public override getGsi2pkValue(): string {
-    return `SECONDARY_INDEX_2_PARTITION_KEY#${this.attribute1.value}`;
+  public override get gsi2pk(): string {
+    return `GSI2_PARTITION_KEY#${this.attribute1}`;
   }
 
-  public override getGsi2skValue(): string {
-    return `SECONDARY_INDEX_2_SORT_KEY#${this.attribute0.value}`;
-  }
-
-  public override toAttributesList(): Attribute[] {
-    const { attribute0, attribute1, creationTime, updateTime } = this;
-    return [attribute0, attribute1, creationTime, updateTime];
-  }
-
-  public static parse(
-    dynamodbItem: Record<string, AttributeValue>,
-  ): MockSchema | undefined {
-    const mockSchema = new MockSchema('', '');
-
-    const { entityName } = mockSchema;
-    entityName.parse(dynamodbItem);
-    if (entityName.value !== mockSchema.getEntityNameValue()) {
-      return undefined;
-    }
-
-    const attributes = mockSchema.toAttributesList();
-    attributes.forEach((attribute) => {
-      attribute.parse(dynamodbItem);
-    });
-
-    return mockSchema;
+  public override get gsi2sk(): string {
+    return `GIS2_SORT_KEY#${this.attribute0}`;
   }
 }
--- test/schema/Schema.test.ts
+++ test/schema/Schema.test.ts
@@ -1,7 +1,5 @@
 import { describe, it, expect, beforeAll, jest, afterAll } from '@jest/globals';
-import { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { MockSchema } from '../__mocks__/MockSchema.js';
-import { Attribute } from '../../src/attribute/index.js';
 
 beforeAll(() => {
   jest.useFakeTimers({
@@ -14,66 +12,26 @@ afterAll(() => {
   jest.useRealTimers();
 });
 
-describe('Schema attributes', () => {
-  it.each([
-    ['attribute0', 'attribute-name-0', 'attribute-value-0'],
-    ['attribute1', 'attribute-name-1', 'attribute-value-1'],
-    ['pk', 'PK', 'PARTITION_KEY#attribute-value-0'],
-    ['sk', 'SK', 'SORT_KEY#attribute-value-1'],
-    ['gsi1pk', 'SK', 'SORT_KEY#attribute-value-1'],
-    ['gsi1sk', 'PK', 'PARTITION_KEY#attribute-value-0'],
-    ['gsi2pk', 'GSI2PK', 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1'],
-    ['gsi2sk', 'GSI2SK', 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0'],
-    ['entityName', '_et', 'MockSchema'],
-    ['creationTime', '_ct', new Date('2021-01-01T00:00:00.000Z')],
-    ['updateTime', '_ut', new Date('2021-01-01T00:00:00.000Z')],
-  ])(`should have %s`, (mockSchemaKey, attributeName, attributeValue) => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-    const attribute = schema[mockSchemaKey as keyof MockSchema] as Attribute;
+describe('Attributes', () => {
+  let schema: MockSchema;
 
-    expect(attribute.name).toBe(attributeName);
-    expect(attribute.value as Attribute).toEqual(attributeValue);
-  });
-});
-
-describe('Parsing DynamoDB item', () => {
-  const dynamodbItem: Record<string, AttributeValue> = {
-    'attribute-name-0': { S: 'attribute-value-0' },
-    'attribute-name-1': { S: 'attribute-value-1' },
-    'PK': { S: 'PARTITION_KEY#attribute-value-0' },
-    'SK': { S: 'SORT_KEY#attribute-value-1' },
-    'GSI2PK': { S: 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1' },
-    'GSI2SK': { S: 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0' },
-    '_et': { S: 'MockSchema' },
-    '_ct': { S: '2022-01-01T00:00:00.000Z' },
-    '_ut': { S: '2023-01-01T00:00:00.000Z' },
-  };
-
-  it.each([
-    ['attribute0', 'attribute-name-0', 'attribute-value-0'],
-    ['attribute1', 'attribute-name-1', 'attribute-value-1'],
-    ['pk', 'PK', 'PARTITION_KEY#attribute-value-0'],
-    ['sk', 'SK', 'SORT_KEY#attribute-value-1'],
-    ['gsi2pk', 'GSI2PK', 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1'],
-    ['gsi2sk', 'GSI2SK', 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0'],
-    ['entityName', '_et', 'MockSchema'],
-    ['creationTime', '_ct', new Date('2022-01-01T00:00:00.000Z')],
-    ['updateTime', '_ut', new Date('2023-01-01T00:00:00.000Z')],
-  ])('should parse %s', (mockSchemaKey, attributeName, attributeValue) => {
-    const schema = MockSchema.parse(dynamodbItem);
-    const attribute = schema![mockSchemaKey as keyof MockSchema] as Attribute;
-
-    expect(schema).toBeDefined();
-    expect(attribute.name).toBe(attributeName);
-    expect(attribute.value).toEqual(attributeValue);
+  beforeAll(() => {
+    schema = new MockSchema('attribute0-value', 'attribute1-value');
   });
 
-  it('should return undefined if entityName is not MockSchema', () => {
-    const schema = MockSchema.parse({
-      ...dynamodbItem,
-      _et: { S: 'NotMockSchema' },
-    });
-
-    expect(schema).toBeUndefined();
+  it.each([
+    ['attribute0', 'attribute0-value'],
+    ['attribute1', 'attribute1-value'],
+    ['creationTime', new Date('2021-01-01T00:00:00.000Z')],
+    ['updateTime', new Date('2021-01-01T00:00:00.000Z')],
+    ['entityName', 'MockSchema'],
+    ['pk', 'PARTITION_KEY#attribute0-value'],
+    ['sk', 'SORT_KEY#attribute1-value'],
+    ['gsi1pk', 'SORT_KEY#attribute1-value'],
+    ['gsi1sk', 'PARTITION_KEY#attribute0-value'],
+    ['gsi2pk', 'GSI2_PARTITION_KEY#attribute1-value'],
+    ['gsi2sk', 'GIS2_SORT_KEY#attribute0-value'],
+  ])(`should have %s`, (attributeName, attributeValue) => {
+    expect(schema).toHaveProperty(attributeName, attributeValue);
   });
 });
