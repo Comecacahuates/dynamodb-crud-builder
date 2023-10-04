7db962364bd8d4ae9f15df967342e216785b1cab
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 22:33:59 -0600

    test(attribute/Attribute): parseDynamodbValue method

--- test/attribute/Attribute.test.ts
+++ test/attribute/Attribute.test.ts
@@ -1,8 +1,9 @@
 import { describe, it, expect } from '@jest/globals';
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { Attribute } from '../../src/attribute/Attribute.js';
+import type { AttributeType } from '../../src/attribute/Attribute.js';
 
-describe('Build DynamoDB value', () => {
+describe('Building DynamoDB value', () => {
   it.each([
     ['string', 'value-0', { S: 'value-0' }],
     [
@@ -29,9 +30,43 @@ describe('Build DynamoDB value', () => {
       },
     ],
   ])(
-    `should DynamoDB value from %s`,
+    `should build DynamoDB value from %s`,
     (_, value: unknown, dynamodbValue: AttributeValue) => {
       expect(Attribute.buildDynamodbValue(value)).toEqual(dynamodbValue);
+    },
+  );
+});
+
+describe('Parsing DynamoDB value', () => {
+  it.each([
+    ['string', { S: 'value-0' }, 'value-0'],
+    [
+      'date string',
+      { S: '2021-01-01T00:00:00.000Z' },
+      new Date('2021-01-01T00:00:00.000Z'),
+    ],
+    ['number', { N: '1' }, 1],
+    ['boolean', { BOOL: true }, true],
+    ['null', { NULL: true }, null],
+    ['number set', { NS: ['1', '2', '3'] }, new Set<number>([1, 2, 3])],
+    ['string set', { SS: ['a', 'b', 'c'] }, new Set<string>(['a', 'b', 'c'])],
+    ['list', { L: [{ N: '1' }, { S: 'a' }, { BOOL: true }] }, [1, 'a', true]],
+    ['empty list', { L: [] }, []],
+    [
+      'object',
+      {
+        M: {
+          a: { N: '1' },
+          b: { M: { c: { BOOL: true } } },
+          d: { L: [{ N: '1' }, { N: '2' }, { N: '3' }] },
+        },
+      },
+      { a: 1, b: { c: true }, d: [1, 2, 3] },
+    ],
+  ])(
+    `should parse %s DynamoDB value`,
+    (_, dynamodbValue: AttributeValue, value: AttributeType) => {
+      expect(Attribute.parseDynamodbValue(dynamodbValue)).toEqual(value);
     },
   );
 });
