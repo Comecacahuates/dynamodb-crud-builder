b8c920e065f3235bfaa6ad72a480694e61fa55b2
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 19:22:04 -0600

    test(schema/Schema)

--- test/__mocks__/MockSchema.ts
+++ test/__mocks__/MockSchema.ts
@@ -1,3 +1,4 @@
+import { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { Schema } from '../../src/schema/Schema.js';
 import { StringAttribute } from '../../src/attribute/index.js';
 
@@ -6,9 +7,13 @@ export class MockSchema extends Schema {
   public readonly attribute1: StringAttribute;
 
   public constructor(attribute0: string, attribute1: string) {
-    super('MockSchema');
+    super();
     this.attribute0 = new StringAttribute('attribute-name-0', attribute0);
     this.attribute1 = new StringAttribute('attribute-name-1', attribute1);
+  }
+
+  public override getEntityNameValue(): string {
+    return 'MockSchema';
   }
 
   public override getPkValue(): string {
@@ -25,5 +30,29 @@ export class MockSchema extends Schema {
 
   public override getGsi2skValue(): string {
     return `SECONDARY_INDEX_2_SORT_KEY#${this.attribute0.value}`;
+  }
+
+  public static parse(
+    dynamodbItem: Record<string, AttributeValue>,
+  ): MockSchema | undefined {
+    const mockSchema = new MockSchema('', '');
+
+    mockSchema.entityName.parse(dynamodbItem);
+    if (mockSchema.entityName.value !== 'MockSchema') {
+      return undefined;
+    }
+
+    const { attribute0, attribute1, creationTime, updateTime } = mockSchema;
+    const attributesToParse = [
+      attribute0,
+      attribute1,
+      creationTime,
+      updateTime,
+    ];
+    attributesToParse.forEach((attribute) => {
+      attribute.parse(dynamodbItem);
+    });
+
+    return mockSchema;
   }
 }
--- test/schema/Schema.test.ts
+++ test/schema/Schema.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect, beforeAll, jest, afterAll } from '@jest/globals';
+import { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { MockSchema } from '../__mocks__/MockSchema.js';
+import { Attribute } from '../../src/attribute/index.js';
 
 beforeAll(() => {
   jest.useFakeTimers({
@@ -13,84 +15,65 @@ afterAll(() => {
 });
 
 describe('Schema attributes', () => {
-  it('should have attribute0', () => {
+  it.each([
+    ['attribute0', 'attribute-name-0', 'attribute-value-0'],
+    ['attribute1', 'attribute-name-1', 'attribute-value-1'],
+    ['pk', 'PK', 'PARTITION_KEY#attribute-value-0'],
+    ['sk', 'SK', 'SORT_KEY#attribute-value-1'],
+    ['gsi1pk', 'SK', 'SORT_KEY#attribute-value-1'],
+    ['gsi1sk', 'PK', 'PARTITION_KEY#attribute-value-0'],
+    ['gsi2pk', 'GSI2PK', 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1'],
+    ['gsi2sk', 'GSI2SK', 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0'],
+    ['entityName', '_et', 'MockSchema'],
+    ['creationTime', '_ct', new Date('2021-01-01T00:00:00.000Z')],
+    ['updateTime', '_ut', new Date('2021-01-01T00:00:00.000Z')],
+  ])(`should have %s`, (mockSchemaKey, attributeName, attributeValue) => {
     const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
+    const attribute = schema[mockSchemaKey as keyof MockSchema] as Attribute;
 
-    expect(schema.attribute0.name).toBe('attribute-name-0');
-    expect(schema.attribute0.value).toBe('attribute-value-0');
+    expect(attribute.name).toBe(attributeName);
+    expect(attribute.value as Attribute).toEqual(attributeValue);
+  });
+});
+
+describe('Parsing DynamoDB item', () => {
+  const dynamodbItem: Record<string, AttributeValue> = {
+    'attribute-name-0': { S: 'attribute-value-0' },
+    'attribute-name-1': { S: 'attribute-value-1' },
+    'PK': { S: 'PARTITION_KEY#attribute-value-0' },
+    'SK': { S: 'SORT_KEY#attribute-value-1' },
+    'GSI2PK': { S: 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1' },
+    'GSI2SK': { S: 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0' },
+    '_et': { S: 'MockSchema' },
+    '_ct': { S: '2022-01-01T00:00:00.000Z' },
+    '_ut': { S: '2023-01-01T00:00:00.000Z' },
+  };
+
+  it.each([
+    ['attribute0', 'attribute-name-0', 'attribute-value-0'],
+    ['attribute1', 'attribute-name-1', 'attribute-value-1'],
+    ['pk', 'PK', 'PARTITION_KEY#attribute-value-0'],
+    ['sk', 'SK', 'SORT_KEY#attribute-value-1'],
+    ['gsi2pk', 'GSI2PK', 'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1'],
+    ['gsi2sk', 'GSI2SK', 'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0'],
+    ['entityName', '_et', 'MockSchema'],
+    ['creationTime', '_ct', new Date('2022-01-01T00:00:00.000Z')],
+    ['updateTime', '_ut', new Date('2023-01-01T00:00:00.000Z')],
+  ])('should parse %s', (mockSchemaKey, attributeName, attributeValue) => {
+    const schema = MockSchema.parse(dynamodbItem);
+    const attribute = schema![mockSchemaKey as keyof MockSchema] as Attribute;
+
+    expect(schema).toBeDefined();
+    expect(attribute.name).toBe(attributeName);
+    expect(attribute.value).toEqual(attributeValue);
   });
 
-  it('should have attribute1', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
+  it('should return undefined if entityName is not MockSchema', () => {
+    const schema = MockSchema.parse({
+      ...dynamodbItem,
+      _et: { S: 'NotMockSchema' },
+    });
 
-    expect(schema.attribute1.name).toBe('attribute-name-1');
-    expect(schema.attribute1.value).toBe('attribute-value-1');
-  });
-
-  it('should have primary index partition key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.pk.name).toBe('PK');
-    expect(schema.pk.value).toBe('PARTITION_KEY#attribute-value-0');
-  });
-
-  it('should have primary index sort key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.sk.name).toBe('SK');
-    expect(schema.sk.value).toBe('SORT_KEY#attribute-value-1');
-  });
-
-  it('should have secondary index 1 partition key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.gsi1pk.name).toBe('SK');
-    expect(schema.gsi1pk.value).toBe('SORT_KEY#attribute-value-1');
-  });
-
-  it('should have secondary index 1 sort key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.gsi1sk.name).toBe('PK');
-    expect(schema.gsi1sk.value).toBe('PARTITION_KEY#attribute-value-0');
-  });
-
-  it('should have secondary index 2 partition key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.gsi2pk.name).toBe('GSI2PK');
-    expect(schema.gsi2pk.value).toBe(
-      'SECONDARY_INDEX_2_PARTITION_KEY#attribute-value-1',
-    );
-  });
-
-  it('should have secondary index 2 sort key', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.gsi2sk.name).toBe('GSI2SK');
-    expect(schema.gsi2sk.value).toBe(
-      'SECONDARY_INDEX_2_SORT_KEY#attribute-value-0',
-    );
-  });
-
-  it('should have entity name', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.entityName.name).toBe('_et');
-    expect(schema.entityName.value).toBe('MockSchema');
-  });
-
-  it('should have creation time', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.creationTime.name).toBe('_ct');
-    expect(schema.creationTime.value).toBeInstanceOf(Date);
-  });
-
-  it('should have update time', () => {
-    const schema = new MockSchema('attribute-value-0', 'attribute-value-1');
-
-    expect(schema.updateTime.name).toBe('_ut');
-    expect(schema.updateTime.value).toBeInstanceOf(Date);
+    expect(schema).toBeUndefined();
   });
 });
