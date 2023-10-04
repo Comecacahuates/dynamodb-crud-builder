f06f9d530f4c3b715dfcba4ca4b32f47079fad25
Adrián Juárez <comecacahuates@yahoo.com>
2023-10-03 22:33:50 -0600

    update(attribute/Attribute): parseDynamodbValue method

--- src/attribute/Attribute.ts
+++ src/attribute/Attribute.ts
@@ -1,12 +1,24 @@
 import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { match, P } from 'ts-pattern';
-import { InvalidAttributeTypeError } from './error/index.js';
+import { z } from 'zod';
+import {
+  InvalidAttributeTypeError,
+  InvalidDynamodbAttributeTypeError,
+} from './error/index.js';
 
 // const AttributeNameSchema = z
 //   .string()
 //   .min(1)
 //   .max(255)
 //   .regex(/^[a-zA-Z0-9._-]+$/);
+
+export type ScalarAttributeType = string | number | boolean | null | Date;
+export type AttributeType =
+  | ScalarAttributeType
+  | Set<string>
+  | Set<number>
+  | AttributeType[]
+  | { [key: string]: AttributeType };
 
 export abstract class Attribute {
   public static buildDynamodbValue(value: unknown): AttributeValue {
@@ -45,5 +57,54 @@ export abstract class Attribute {
           ]),
         ),
       }));
+  }
+
+  public static parseDynamodbValue(value: AttributeValue): AttributeType {
+    const isValidDate = (value: unknown) => {
+      const dateParsingSchema = z.string().datetime();
+      const parsingResult = dateParsingSchema.safeParse(value);
+      return parsingResult.success;
+    };
+
+    const isValidNumber = (value: unknown) => {
+      const numberParsingSchema = z.number({ coerce: true });
+      const parsingResult = numberParsingSchema.safeParse(value);
+      return parsingResult.success;
+    };
+
+    return match<AttributeValue, AttributeType>(value)
+      .with({ S: P.select(P.when(isValidDate)) }, (string) => new Date(string!))
+
+      .with({ S: P.select(P.string) }, (string) => string!)
+
+      .with({ N: P.select(P.when(isValidNumber)) }, (number) => Number(number))
+
+      .with({ BOOL: P.select(P.boolean) }, (boolean) => boolean)
+
+      .with({ NULL: true }, () => null)
+
+      .with(
+        { NS: P.select(P.array(P.when(isValidNumber))) },
+        (stringNumbers) => new Set<number>(stringNumbers.map(Number)),
+      )
+
+      .with({ SS: P.select(P.array(P.string)) }, (strings) => new Set(strings))
+
+      .with({ L: P.select(P.array(P.any)) }, (list) =>
+        list.map((item) => this.parseDynamodbValue(item)),
+      )
+
+      .with({ M: P.select() }, (object) =>
+        Object.fromEntries(
+          Object.entries(object!).map(([key, value]) => [
+            key,
+            this.parseDynamodbValue(value),
+          ]),
+        ),
+      )
+
+      .otherwise((dynamodbValue) => {
+        throw new InvalidDynamodbAttributeTypeError(dynamodbValue);
+      });
   }
 }
