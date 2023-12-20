a1d575a040a28d71cb2c1cfcbdb4826390bbd3b1
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-20 14:45:28 -0600

    update(src/attribute-value/building): buildStringSetAttributeValue function

--- src/attribute-value/building.ts
+++ src/attribute-value/building.ts
@@ -1,5 +1,7 @@
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
 
+export const NULL_ATTRIBUTE_VALUE: AttributeValue.NULLMember = { NULL: true };
+
 export function buildStringAttributeValue(
   attributeValue: string,
 ): AttributeValue.SMember {
@@ -16,4 +18,10 @@ export function buildBooleanAttributeValue(
   attributeValue: boolean,
 ): AttributeValue.BOOLMember {
   return { BOOL: attributeValue };
+}
+
+export function buildStringSetAttributeValue(
+  attributeValue: Set<string>,
+): AttributeValue.SSMember {
+  return { SS: [...attributeValue] };
 }
