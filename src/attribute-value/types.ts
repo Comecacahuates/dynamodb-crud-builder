341cc8ea892d3fdf373b907a4e2ee7bc8080ea1d
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-20 16:50:07 -0600

    add(src/attribute-value/types)

---
+++ src/attribute-value/types.ts
@@ -0,0 +1,11 @@
+export type AttributeType =
+  | string
+  | number
+  | null
+  | boolean
+  | Uint8Array
+  | Set<string>
+  | Set<number>
+  | Set<Uint8Array>
+  | AttributeType[]
+  | { [key: string]: AttributeType };
