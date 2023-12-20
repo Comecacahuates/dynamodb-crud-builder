fe1d2e062f3c3aefaeca1868c7ade9fd605cc31a
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-20 16:50:52 -0600

    test(src/attribute-value/building): build function

--- test/attribute-value/building.test.ts
+++ test/attribute-value/building.test.ts
@@ -1,19 +1,11 @@
 import { describe, it, expect } from '@jest/globals';
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
-import {
-  buildString,
-  buildNumber,
-  buildBoolean,
-  buildBinary,
-  buildStringSet,
-  buildNumberSet,
-  buildBinarySet,
-} from '../../src/attribute-value/building.js';
+import * as Attribute from '../../src/attribute-value/index.js';
 
 describe('Building string DynamoDB attribute value', () => {
   it('should build string attribute value', () => {
     const attributeValue: AttributeValue.SMember =
-      buildString('attribute-value');
+      Attribute.buildString('attribute-value');
 
     expect(attributeValue).toEqual({ S: 'attribute-value' });
   });
@@ -21,7 +13,7 @@ describe('Building string DynamoDB attribute value', () => {
 
 describe('Building number DynamoDB attribute value', () => {
   it('should build number attribute value', () => {
-    const attributeValue: AttributeValue.NMember = buildNumber(123);
+    const attributeValue: AttributeValue.NMember = Attribute.buildNumber(123);
 
     expect(attributeValue).toEqual({ N: '123' });
   });
@@ -29,7 +21,8 @@ describe('Building number DynamoDB attribute value', () => {
 
 describe('Building boolean DynamoDB attribute value', () => {
   it('should build boolean attribute value', () => {
-    const attributeValue: AttributeValue.BOOLMember = buildBoolean(true);
+    const attributeValue: AttributeValue.BOOLMember =
+      Attribute.buildBoolean(true);
 
     expect(attributeValue).toEqual({ BOOL: true });
   });
@@ -37,7 +30,7 @@ describe('Building boolean DynamoDB attribute value', () => {
 
 describe('Building binary DynamoDB attribute value', () => {
   it('should build binary attribute value', () => {
-    const attributeValue: AttributeValue.BMember = buildBinary(
+    const attributeValue: AttributeValue.BMember = Attribute.buildBinary(
       new Uint8Array([1, 2, 3]),
     );
 
@@ -47,7 +40,7 @@ describe('Building binary DynamoDB attribute value', () => {
 
 describe('Building string set DynamoDB attribute value', () => {
   it('should build string set attribute value', () => {
-    const attributeValue: AttributeValue.SSMember = buildStringSet(
+    const attributeValue: AttributeValue.SSMember = Attribute.buildStringSet(
       new Set(['value1', 'value2']),
     );
 
@@ -57,7 +50,7 @@ describe('Building string set DynamoDB attribute value', () => {
 
 describe('Building number set DynamoDB attribute value', () => {
   it('should build number set attribute value', () => {
-    const attributeValue: AttributeValue.NSMember = buildNumberSet(
+    const attributeValue: AttributeValue.NSMember = Attribute.buildNumberSet(
       new Set([123, 456]),
     );
 
@@ -67,12 +60,403 @@ describe('Building number set DynamoDB attribute value', () => {
 
 describe('Build binary set DynamoDB attribute value', () => {
   it('should build binary set attribute value', () => {
-    const attributeValue: AttributeValue.BSMember = buildBinarySet(
+    const attributeValue: AttributeValue.BSMember = Attribute.buildBinarySet(
       new Set([new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])]),
     );
 
     expect(attributeValue).toEqual({
       BS: [new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])],
+    });
+  });
+});
+
+describe('Build DynamoDB attribute value', () => {
+  it('should build null attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(null);
+
+    expect(attributeValue).toEqual({ NULL: true });
+  });
+
+  it('should build string attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build('attribute-value');
+
+    expect(attributeValue).toEqual({ S: 'attribute-value' });
+  });
+
+  it('should build number attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(123);
+
+    expect(attributeValue).toEqual({ N: '123' });
+  });
+
+  it('should build boolean attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(true);
+
+    expect(attributeValue).toEqual({ BOOL: true });
+  });
+
+  it('should build binary attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(
+      new Uint8Array([1, 2, 3]),
+    );
+
+    expect(attributeValue).toEqual({ B: new Uint8Array([1, 2, 3]) });
+  });
+
+  it('should build string set attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(
+      new Set(['value1', 'value2']),
+    );
+
+    expect(attributeValue).toEqual({ SS: ['value1', 'value2'] });
+  });
+
+  it('should build number set attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(new Set([123, 456]));
+
+    expect(attributeValue).toEqual({ NS: ['123', '456'] });
+  });
+
+  it('should build binary set attribute value', () => {
+    const attributeValue: AttributeValue = Attribute.build(
+      new Set([new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])]),
+    );
+
+    expect(attributeValue).toEqual({
+      BS: [new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])],
+    });
+  });
+
+  describe('Building list DynamoDB attribute value', () => {
+    it('should build empty list attribute value', () => {
+      const attributeValue: AttributeValue = Attribute.build([]);
+
+      expect(attributeValue).toEqual({ L: [] });
+    });
+
+    it('should build list attribute value only with nulls', () => {
+      const attributeValue: AttributeValue = Attribute.build([null, null]);
+
+      expect(attributeValue).toEqual({
+        L: [{ NULL: true }, { NULL: true }],
+      });
+    });
+
+    it('should build list attribute value only with strings', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        'value1',
+        'value2',
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [{ S: 'value1' }, { S: 'value2' }],
+      });
+    });
+
+    it('should build list attribute value only with numbers', () => {
+      const attributeValue: AttributeValue = Attribute.build([123, 456]);
+
+      expect(attributeValue).toEqual({
+        L: [{ N: '123' }, { N: '456' }],
+      });
+    });
+
+    it('should build list attribute value only with booleans', () => {
+      const attributeValue: AttributeValue = Attribute.build([true, false]);
+
+      expect(attributeValue).toEqual({
+        L: [{ BOOL: true }, { BOOL: false }],
+      });
+    });
+
+    it('should build list attribute value only with binaries', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        new Uint8Array([1, 2, 3]),
+        new Uint8Array([4, 5, 6]),
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [{ B: new Uint8Array([1, 2, 3]) }, { B: new Uint8Array([4, 5, 6]) }],
+      });
+    });
+
+    it('should build list attribute value only with strings sets', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        new Set(['value1', 'value2']),
+        new Set(['value3', 'value4']),
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [{ SS: ['value1', 'value2'] }, { SS: ['value3', 'value4'] }],
+      });
+    });
+
+    it('should build list attribute value only with number sets', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        new Set([123, 456]),
+        new Set([789, 101112]),
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [{ NS: ['123', '456'] }, { NS: ['789', '101112'] }],
+      });
+    });
+
+    it('should build list attribute value only with binary sets', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        new Set([new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])]),
+        new Set([new Uint8Array([7, 8, 9]), new Uint8Array([10, 11, 12])]),
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [
+          {
+            BS: [new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])],
+          },
+          {
+            BS: [new Uint8Array([7, 8, 9]), new Uint8Array([10, 11, 12])],
+          },
+        ],
+      });
+    });
+
+    it('should build list attribute value only with lists', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        ['value1', 'value2'],
+        ['value3', 'value4'],
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [
+          { L: [{ S: 'value1' }, { S: 'value2' }] },
+          { L: [{ S: 'value3' }, { S: 'value4' }] },
+        ],
+      });
+    });
+
+    it('should build list attribute value only with objects', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        { attr1: 'value1', attr2: 'value2' },
+        { attr3: 'value3', attr4: 'value4' },
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [
+          { M: { attr1: { S: 'value1' }, attr2: { S: 'value2' } } },
+          { M: { attr3: { S: 'value3' }, attr4: { S: 'value4' } } },
+        ],
+      });
+    });
+
+    it('should build list attribute value with different types of items', () => {
+      const attributeValue: AttributeValue = Attribute.build([
+        null,
+        'value1',
+        123,
+        true,
+        new Uint8Array([1, 2, 3]),
+        new Set(['value2', 'value3']),
+        ['value4', 'value5'],
+        {
+          attr1: null,
+          attr2: 'value6',
+          attr3: 456,
+          attr4: true,
+          attr5: new Uint8Array([4, 5, 6]),
+          attr6: new Set(['value7', 'value8']),
+          attr7: ['value9', 'value10'],
+        },
+      ]);
+
+      expect(attributeValue).toEqual({
+        L: [
+          { NULL: true },
+          { S: 'value1' },
+          { N: '123' },
+          { BOOL: true },
+          { B: new Uint8Array([1, 2, 3]) },
+          { SS: ['value2', 'value3'] },
+          { L: [{ S: 'value4' }, { S: 'value5' }] },
+          {
+            M: {
+              attr1: { NULL: true },
+              attr2: { S: 'value6' },
+              attr3: { N: '456' },
+              attr4: { BOOL: true },
+              attr5: { B: new Uint8Array([4, 5, 6]) },
+              attr6: { SS: ['value7', 'value8'] },
+              attr7: { L: [{ S: 'value9' }, { S: 'value10' }] },
+            },
+          },
+        ],
+      });
+    });
+  });
+
+  describe('Building object DynamoDB attribute value', () => {
+    it('should build empty object attribute value', () => {
+      const attributeValue: AttributeValue = Attribute.build({});
+
+      expect(attributeValue).toEqual({ M: {} });
+    });
+
+    it('should build object attribute value only with nulls', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: null,
+        attr2: null,
+      });
+
+      expect(attributeValue).toEqual({
+        M: { attr1: { NULL: true }, attr2: { NULL: true } },
+      });
+    });
+
+    it('should build object attribute value only with strings', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: 'value1',
+        attr2: 'value2',
+      });
+
+      expect(attributeValue).toEqual({
+        M: { attr1: { S: 'value1' }, attr2: { S: 'value2' } },
+      });
+    });
+
+    it('should build object attribute value only with numbers', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: 123,
+        attr2: 456,
+      });
+
+      expect(attributeValue).toEqual({
+        M: { attr1: { N: '123' }, attr2: { N: '456' } },
+      });
+    });
+
+    it('should build object attribute value only with booleans', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: true,
+        attr2: false,
+      });
+
+      expect(attributeValue).toEqual({
+        M: { attr1: { BOOL: true }, attr2: { BOOL: false } },
+      });
+    });
+
+    it('should build object attribute value only with binaries', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: new Uint8Array([1, 2, 3]),
+        attr2: new Uint8Array([4, 5, 6]),
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { B: new Uint8Array([1, 2, 3]) },
+          attr2: { B: new Uint8Array([4, 5, 6]) },
+        },
+      });
+    });
+
+    it('should build object attribute value only with strings sets', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: new Set(['value1', 'value2']),
+        attr2: new Set(['value3', 'value4']),
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { SS: ['value1', 'value2'] },
+          attr2: { SS: ['value3', 'value4'] },
+        },
+      });
+    });
+
+    it('should build object attribute value only with number sets', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: new Set([123, 456]),
+        attr2: new Set([789, 101112]),
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { NS: ['123', '456'] },
+          attr2: { NS: ['789', '101112'] },
+        },
+      });
+    });
+
+    it('should build object attribute value only with binary sets', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: new Set([new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])]),
+        attr2: new Set([
+          new Uint8Array([7, 8, 9]),
+          new Uint8Array([10, 11, 12]),
+        ]),
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: {
+            BS: [new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6])],
+          },
+          attr2: {
+            BS: [new Uint8Array([7, 8, 9]), new Uint8Array([10, 11, 12])],
+          },
+        },
+      });
+    });
+
+    it('should build object attribute value only with lists', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: ['value1', 'value2'],
+        attr2: ['value3', 'value4'],
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { L: [{ S: 'value1' }, { S: 'value2' }] },
+          attr2: { L: [{ S: 'value3' }, { S: 'value4' }] },
+        },
+      });
+    });
+
+    it('should build object attribute value with different types of items', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: null,
+        attr2: 'value1',
+        attr3: 123,
+        attr4: true,
+        attr5: new Uint8Array([1, 2, 3]),
+        attr6: new Set(['value2', 'value3']),
+        attr7: ['value4', 'value5'],
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { NULL: true },
+          attr2: { S: 'value1' },
+          attr3: { N: '123' },
+          attr4: { BOOL: true },
+          attr5: { B: new Uint8Array([1, 2, 3]) },
+          attr6: { SS: ['value2', 'value3'] },
+          attr7: { L: [{ S: 'value4' }, { S: 'value5' }] },
+        },
+      });
+    });
+
+    it('should build object attribute value with nested objects', () => {
+      const attributeValue: AttributeValue = Attribute.build({
+        attr1: { attr2: { attr3: 'value1' } },
+        attr4: { attr5: { attr6: 'value2' } },
+      });
+
+      expect(attributeValue).toEqual({
+        M: {
+          attr1: { M: { attr2: { M: { attr3: { S: 'value1' } } } } },
+          attr4: { M: { attr5: { M: { attr6: { S: 'value2' } } } } },
+        },
+      });
     });
   });
 });
