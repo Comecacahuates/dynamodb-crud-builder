aba8eae417a752cbeebbd114b000a972be0b7bce
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-20 14:31:32 -0600

    test(src/attribute-value/building): buildNumberAttributeValue

--- test/attribute-value/building.test.ts
+++ test/attribute-value/building.test.ts
@@ -1,6 +1,9 @@
 import { describe, it, expect } from '@jest/globals';
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
-import { buildStringAttributeValue } from '../../src/attribute-value/building.js';
+import {
+  buildStringAttributeValue,
+  buildNumberAttributeValue,
+} from '../../src/attribute-value/building.js';
 
 describe('Building string DynamoDB attribute value', () => {
   it('should build string attribute value', () => {
@@ -8,5 +11,14 @@ describe('Building string DynamoDB attribute value', () => {
       buildStringAttributeValue('attribute-value');
 
     expect(attributeValue).toEqual({ S: 'attribute-value' });
+  });
+});
+
+describe('Building number DynamoDB attribute value', () => {
+  it('should build number attribute value', () => {
+    const attributeValue: AttributeValue.NMember =
+      buildNumberAttributeValue(123);
+
+    expect(attributeValue).toEqual({ N: '123' });
   });
 });
