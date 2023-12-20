39d524c5dbbc57af215bd3b5cfbfebfef4557815
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-20 16:50:42 -0600

    update(src/attribute-value/building): build function

--- src/attribute-value/building.ts
+++ src/attribute-value/building.ts
@@ -1,4 +1,6 @@
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
+import { match, P } from 'ts-pattern';
+import { AttributeType } from './types.js';
 
 export const NULL: AttributeValue.NULLMember = { NULL: true };
 
@@ -38,4 +40,37 @@ export function buildBinarySet(
   attributeValue: Set<Uint8Array>,
 ): AttributeValue.BSMember {
   return { BS: [...attributeValue] };
+}
+
+export function build(value: AttributeType): AttributeValue {
+  return match(value)
+    .with(null, () => NULL)
+
+    .with(P.string, buildString)
+
+    .with(P.number, buildNumber)
+
+    .with(P.boolean, buildBoolean)
+
+    .with(P.instanceOf(Uint8Array), buildBinary)
+
+    .with(P.set(P.string), buildStringSet)
+
+    .with(P.set(P.number), buildNumberSet)
+
+    .with(P.set(P.instanceOf(Uint8Array)), buildBinarySet)
+
+    .with(P.array(), (value: AttributeType[]) => ({
+      L: value.map((item) => build(item)),
+    }))
+
+    .otherwise((value: { [key: string]: AttributeType }) => ({
+      M: Object.entries(value).reduce(
+        (attributeValue, [entryKey, entryValue]) => ({
+          ...attributeValue,
+          [entryKey]: build(entryValue),
+        }),
+        {},
+      ),
+    }));
 }
