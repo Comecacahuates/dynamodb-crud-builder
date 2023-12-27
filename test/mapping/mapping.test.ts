17fd06a7187399518fcd85a5b5ee256ac713da29
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-27 15:33:32 -0600

    test(mapping/mapping): buildReverseItemMapping function

--- test/mapping/mapping.test.ts
+++ test/mapping/mapping.test.ts
@@ -133,7 +133,7 @@ describe('Item mapping', () => {
     const itemMapping: Mapping.ItemMapping = {
       attribute: {
         mappedName: 'mapped-name',
-        nestedMapping: {
+        nestedAttributesMapping: {
           'nested-attribute-name': {
             mappedName: 'mapped-nested-attribute-name',
           },
@@ -179,7 +179,7 @@ describe('Item mapping', () => {
       'attribute-7': { mappedName: 'a7' },
       'attribute-8': {
         mappedName: 'a8',
-        nestedMapping: {
+        nestedAttributesMapping: {
           'nested-attribute': {
             mappedName: 'a8-nested',
           },
@@ -216,5 +216,111 @@ describe('Item mapping', () => {
     const actualMappedItem = Mapping.mapItem(itemToMap, itemMapping);
 
     expect(actualMappedItem).toEqual(expectedMappedItem);
+  });
+});
+
+describe('Reverse item mapping', () => {
+  it('should build reverse item mapping with single attribute', () => {
+    const itemMapping: Mapping.ItemMapping = {
+      attribute: { mappedName: 'a' },
+    };
+
+    const expectedReverseItemMapping = {
+      a: { mappedName: 'attribute' },
+    };
+    const actualReverseItemMapping =
+      Mapping.buildReverseItemMapping(itemMapping);
+
+    expect(actualReverseItemMapping).toEqual(expectedReverseItemMapping);
+  });
+
+  it('should build reverse item mapping with nested attribute', () => {
+    const itemMapping: Mapping.ItemMapping = {
+      attribute: {
+        mappedName: 'a',
+        nestedAttributesMapping: {
+          'nested-attribute': {
+            mappedName: 'nested-a',
+          },
+        },
+      },
+    };
+
+    const expectedReverseItemMapping = {
+      a: {
+        mappedName: 'attribute',
+        nestedAttributesMapping: {
+          'nested-a': { mappedName: 'nested-attribute' },
+        },
+      },
+    };
+    const actualReverseItemMapping =
+      Mapping.buildReverseItemMapping(itemMapping);
+
+    console.log('ACTUAL', JSON.stringify(actualReverseItemMapping, null, 2));
+    console.log(
+      'EXPECTED',
+      JSON.stringify(expectedReverseItemMapping, null, 2),
+    );
+
+    expect(actualReverseItemMapping).toEqual(expectedReverseItemMapping);
+  });
+
+  it('should build reverse item mapping with multiple attributes', () => {
+    const itemMapping: Mapping.ItemMapping = {
+      'attribute-1': { mappedName: 'a1' },
+      'attribute-2': { mappedName: 'a2' },
+      'attribute-3': { mappedName: 'a3' },
+    };
+
+    const expectedReverseItemMapping = {
+      a1: { mappedName: 'attribute-1' },
+      a2: { mappedName: 'attribute-2' },
+      a3: { mappedName: 'attribute-3' },
+    };
+    const actualReverseItemMapping =
+      Mapping.buildReverseItemMapping(itemMapping);
+
+    expect(actualReverseItemMapping).toEqual(expectedReverseItemMapping);
+  });
+
+  it('should build reverse item mapping with nested attributes', () => {
+    const itemMapping: Mapping.ItemMapping = {
+      'attribute-1': {
+        mappedName: 'a1',
+        nestedAttributesMapping: {
+          'nested-attribute-1': {
+            mappedName: 'nested-a1',
+          },
+        },
+      },
+      'attribute-2': {
+        mappedName: 'a2',
+        nestedAttributesMapping: {
+          'nested-attribute-2': {
+            mappedName: 'nested-a2',
+          },
+        },
+      },
+    };
+
+    const expectedReverseItemMapping = {
+      a1: {
+        mappedName: 'attribute-1',
+        nestedAttributesMapping: {
+          'nested-a1': { mappedName: 'nested-attribute-1' },
+        },
+      },
+      a2: {
+        mappedName: 'attribute-2',
+        nestedAttributesMapping: {
+          'nested-a2': { mappedName: 'nested-attribute-2' },
+        },
+      },
+    };
+    const actualReverseItemMapping =
+      Mapping.buildReverseItemMapping(itemMapping);
+
+    expect(actualReverseItemMapping).toEqual(expectedReverseItemMapping);
   });
 });
