6fd189921260b77d0bdf458c24aad4232ddd94b7
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-28 02:04:19 -0600

    test(expressions/expression-attribute-values): buildFromAttributePathAndValue function

--- test/expressions/expression-attribute-values.test.ts
+++ test/expressions/expression-attribute-values.test.ts
@@ -8,3 +8,30 @@ describe('Building placeholder', () => {
     expect(placeholder).toBe(':id');
   });
 });
+
+describe('Building from attribute name and value', () => {
+  it('should return expression attribute value', () => {
+    const fromAttributeNameAndValue =
+      ExpressionAttributeValues.buildFromAttributeNameAndValue('id', {
+        S: 'id',
+      });
+
+    expect(fromAttributeNameAndValue).toEqual({
+      ':id': { S: 'id' },
+    });
+  });
+});
+
+describe('Building from attribute path and value', () => {
+  it('should return expression attribute value', () => {
+    const fromAttributePathAndValue =
+      ExpressionAttributeValues.buildFromAttributePathAndValue(
+        ['a', 'b', 'c'],
+        { S: 'id' },
+      );
+
+    expect(fromAttributePathAndValue).toEqual({
+      ':abc': { S: 'id' },
+    });
+  });
+});
