1fa6045e6f6defcbe2a6f9f1ed517a173a63c22f
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-28 01:29:13 -0600

    test(mapping/mapping): mapAttributePath function

--- test/mapping/mapping.test.ts
+++ test/mapping/mapping.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from '@jest/globals';
 import * as Mapping from '../../src/mapping/index.js';
+import { PathMappingError } from '../../src/errors/index.js';
 
 describe('Item mapping', () => {
   it('should map item with single null attribute', () => {
@@ -322,5 +323,66 @@ describe('Reverse item mapping', () => {
       Mapping.buildReverseItemMapping(itemMapping);
 
     expect(actualReverseItemMapping).toEqual(expectedReverseItemMapping);
+  });
+});
+
+describe('Attribute path mapping', () => {
+  const itemMapping: Mapping.ItemMapping = {
+    attribute1: {
+      mappedName: 'a1',
+    },
+    attribute2: {
+      mappedName: 'a2',
+      nestedAttributesMapping: {
+        'nested-attribute2': {
+          mappedName: 'nested-a2',
+        },
+      },
+    },
+    attribute3: {
+      mappedName: 'a3',
+      nestedAttributesMapping: {
+        'nested-attribute3': {
+          mappedName: 'nested-a3',
+          nestedAttributesMapping: {
+            'nested-nested-attribute3': {
+              mappedName: 'nested-nested-a3',
+            },
+          },
+        },
+      },
+    },
+  };
+
+  it.each([
+    ['simple path', ['attribute1'], ['a1']],
+    [
+      'one level nesting path',
+      ['attribute2', 'nested-attribute2'],
+      ['a2', 'nested-a2'],
+    ],
+    [
+      'two level nesting path',
+      ['attribute3', 'nested-attribute3', 'nested-nested-attribute3'],
+      ['a3', 'nested-a3', 'nested-nested-a3'],
+    ],
+  ])(
+    'should map %s',
+    (_, pathToMap: Array<string>, expectedMappedPath: Array<string>) => {
+      const mappedAttributePath = Mapping.mapAttributePath(
+        pathToMap,
+        itemMapping,
+      );
+
+      expect(mappedAttributePath).toEqual(expectedMappedPath);
+    },
+  );
+
+  it('should throw error if attribute path is not defined', () => {
+    const pathToMap = ['attribute4'];
+
+    expect(() => Mapping.mapAttributePath(pathToMap, itemMapping)).toThrow(
+      PathMappingError,
+    );
   });
 });
