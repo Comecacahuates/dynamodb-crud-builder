1ac13b9f102b6876b80a4e2c46e3d67295a69592
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-28 01:55:15 -0600

    test(expressions/expression-attribute-names): buildFromAttributePath function

--- test/expressions/expression-attribute-names.test.ts
+++ test/expressions/expression-attribute-names.test.ts
@@ -10,10 +10,34 @@ describe('Building placeholder', () => {
 });
 
 describe('Building from attribute name', () => {
-  it('should build from attribute name', () => {
+  it('should return expression attribute name', () => {
     const fromAttributeName =
       ExpressionAttributeNames.buildFromAttributeName('id');
 
     expect(fromAttributeName).toEqual({ '#id': 'id' });
+  });
+});
+
+describe('Building from attribute path', () => {
+  it('should return expression attribute names from all parts of the path', () => {
+    const fromAttributePath = ExpressionAttributeNames.buildFromAttributePath([
+      'a',
+      'b',
+      'c',
+    ]);
+
+    expect(fromAttributePath).toEqual({
+      '#a': 'a',
+      '#b': 'b',
+      '#c': 'c',
+    });
+  });
+
+  it('should return empty object for empty path', () => {
+    const fromAttributePath = ExpressionAttributeNames.buildFromAttributePath(
+      [],
+    );
+
+    expect(fromAttributePath).toEqual({});
   });
 });
