0696b7bae58d33eb08c846db2a0c806605d9f6cd
Adrián Juárez <comecacahuates@yahoo.com>
2023-12-28 01:49:00 -0600

    add(expressions/expression-attribute-names): buildFromAttributeName function

--- src/expressions/expression-attribute-names.ts
+++ src/expressions/expression-attribute-names.ts
@@ -1,3 +1,12 @@
 export function buildPlaceholder(attributeName: string): string {
   return `#${attributeName}`;
 }
+
+export function buildFromAttributeName(
+  attributeName: string,
+): Record<string, string> {
+  const placeholder = buildPlaceholder(attributeName);
+  return {
+    [placeholder]: attributeName,
+  };
+}
