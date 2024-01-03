e38c4fddee87c9447dc8c6e213dfe72e3a72096b
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-03 14:43:35 -0600

    refactor(expressions/update/statements/add): rename buildAddStatement function

--- test/expressions/update/statements/add.test.ts
+++ test/expressions/update/statements/add.test.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from '@jest/globals';
 import { Add } from '../../../../src/expressions/update/statements/index.js';
 
-describe('Building add number statement', () => {
+describe('Building add  statement', () => {
   it('should return statement string', () => {
-    const statement = Add.buildAddNumberStatement(['a', 'b', 'c']);
+    const statement = Add.buildAddStatement(['a', 'b', 'c']);
 
     expect(statement).toBe('#a.#b.#c :abc');
   });
