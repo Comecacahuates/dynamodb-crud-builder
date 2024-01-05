20d7ed8733d9d7e724b66dd7ac2812742b86f60e
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-04 19:07:47 -0600

    test(expressions/update/statement/delete): test attribute path with indexes

--- test/expressions/update/statements/delete.test.ts
+++ test/expressions/update/statements/delete.test.ts
@@ -4,17 +4,17 @@ import { Delete } from '../../../../src/expressions/update/statements/index.js';
 describe('Building statement to delete', () => {
   describe('Deleting from set attribute', () => {
     it('should return statement string', () => {
-      const statement = Delete.buildStatementToDelete(['a', 'b', 'c']);
+      const statement = Delete.buildStatementToDelete([
+        'a',
+        'b',
+        'c',
+        1,
+        'd',
+        'e',
+        2,
+      ]);
 
-      expect(statement).toBe('#a.#b.#c :abc');
-    });
-  });
-
-  describe('Deleting from set list item', () => {
-    it('should return statement string', () => {
-      const statement = Delete.buildStatementToDelete(['a', 'b', 'c'], 1);
-
-      expect(statement).toBe('#a.#b.#c[1] :abc1');
+      expect(statement).toBe('#a.#b.#c[1].#d.#e[2] :abc1de2');
     });
   });
 });
