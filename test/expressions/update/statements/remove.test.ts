659b2894c163ba134c85e26d3aabc2c8f047fa0e
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-04 19:08:45 -0600

    test(expressions/update/statement/remove): test attribute path with indexes

--- test/expressions/update/statements/remove.test.ts
+++ test/expressions/update/statements/remove.test.ts
@@ -4,17 +4,17 @@ import { Remove } from '../../../../src/expressions/update/statements/index.js';
 describe('Building statement to remove attribute', () => {
   describe('Removing attribute', () => {
     it('should return statement string', () => {
-      const statement = Remove.buildStatementToRemove(['a', 'b', 'c']);
+      const statement = Remove.buildStatementToRemove([
+        'a',
+        'b',
+        'c',
+        1,
+        'd',
+        'e',
+        2,
+      ]);
 
-      expect(statement).toBe('#a.#b.#c');
-    });
-  });
-
-  describe('Removing item from', () => {
-    it('should return statement string', () => {
-      const statement = Remove.buildStatementToRemove(['a', 'b', 'c'], 1);
-
-      expect(statement).toBe('#a.#b.#c[1]');
+      expect(statement).toBe('#a.#b.#c[1].#d.#e[2]');
     });
   });
 });
