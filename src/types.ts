a46608879b014336126d8beb07ab6229ec995bb1
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-04 18:31:45 -0600

    feat(types): redefine AttributePath

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { type AttributeValue } from '@aws-sdk/client-dynamodb';
 
-export type AttributePath = Array<string>;
+export type AttributePath = Array<string | number>;
 
 export type Item = Record<string, AttributeValue>;
 
