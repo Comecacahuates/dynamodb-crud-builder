a8c1a2eaedca64d524e005e22f4b7fe6f7cb573b
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-12 13:33:50 -0600

    feat(document-path/formatting): formatDocumentPath function

--- src/document-path/formatting.ts
+++ src/document-path/formatting.ts
@@ -1,4 +1,4 @@
-import { type DocumentPathItem } from './types.js';
+import { type DocumentPathItem, type DocumentPath } from './types.js';
 
 export function formatDocumentPathItem(
   documentPathItem: DocumentPathItem,
@@ -10,4 +10,8 @@ export function formatDocumentPathItem(
   }
 
   return attributeName;
+}
+
+export function formatDocumentPath(documentPath: DocumentPath): string {
+  return documentPath.map(formatDocumentPathItem).join('.');
 }
--- test/document-path/formatting.test.ts
+++ test/document-path/formatting.test.ts
@@ -1,6 +1,12 @@
 import { describe, it, expect } from '@jest/globals';
-import { formatDocumentPathItem } from '../../src/document-path/formatting.js';
-import { type DocumentPathItem } from '../../src/document-path/index.js';
+import {
+  formatDocumentPathItem,
+  formatDocumentPath,
+} from '../../src/document-path/formatting.js';
+import {
+  type DocumentPathItem,
+  type DocumentPath,
+} from '../../src/document-path/index.js';
 
 describe('Formatting document path item', () => {
   type TestCase = {
@@ -33,4 +39,64 @@ describe('Formatting document path item', () => {
       );
     },
   );
+});
+
+describe('Formatting document path', () => {
+  type TestCase = {
+    testName: string;
+    documentPath: DocumentPath;
+    formattedDocumentPath: string;
+  };
+
+  const testCases: Array<TestCase> = [
+    {
+      testName: 'should format document path with one item with no index',
+      documentPath: [{ attributeName: 'attr0' }],
+      formattedDocumentPath: 'attr0',
+    },
+    {
+      testName: 'should format document path with five items with no index',
+      documentPath: [
+        { attributeName: 'attr0' },
+        { attributeName: 'attr1', index: 0 },
+        { attributeName: 'attr2' },
+        { attributeName: 'attr3', index: 1 },
+        { attributeName: 'attr4' },
+      ],
+      formattedDocumentPath: 'attr0.attr1[0].attr2.attr3[1].attr4',
+    },
+    {
+      testName: 'should format document path with one item with index',
+      documentPath: [{ attributeName: 'attr0', index: 0 }],
+      formattedDocumentPath: 'attr0[0]',
+    },
+    {
+      testName: 'should format document path with five items with index',
+      documentPath: [
+        { attributeName: 'attr0', index: 0 },
+        { attributeName: 'attr1', index: 1 },
+        { attributeName: 'attr2', index: 2 },
+        { attributeName: 'attr3', index: 3 },
+        { attributeName: 'attr4', index: 4 },
+      ],
+      formattedDocumentPath: 'attr0[0].attr1[1].attr2[2].attr3[3].attr4[4]',
+    },
+    {
+      testName: 'should format document path with five items with mixed index',
+      documentPath: [
+        { attributeName: 'attr0' },
+        { attributeName: 'attr1', index: 1 },
+        { attributeName: 'attr2' },
+        { attributeName: 'attr3', index: 3 },
+        { attributeName: 'attr4', index: 4 },
+      ],
+      formattedDocumentPath: 'attr0.attr1[1].attr2.attr3[3].attr4[4]',
+    },
+  ];
+
+  it.each(testCases)('$testName', ({ documentPath, formattedDocumentPath }) => {
+    const actualFormattedDocumentPath = formatDocumentPath(documentPath);
+
+    expect(actualFormattedDocumentPath).toEqual(formattedDocumentPath);
+  });
 });
