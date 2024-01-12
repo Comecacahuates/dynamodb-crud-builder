0736af31d089fc5a3ec161dab3ed4d61bc03be27
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-12 15:55:42 -0600

    feat(mapping/document-path-mapping): mapDocumentPath function

--- src/mapping/document-path-mapping.ts
+++ src/mapping/document-path-mapping.ts
@@ -1,6 +1,12 @@
 import { type MappingSchema } from './types.js';
-import { type DocumentPathItem } from '../document-path/index.js';
-import { DocumentPathItemMappingError } from '../errors/index.js';
+import {
+  type DocumentPathItem,
+  type DocumentPath,
+} from '../document-path/index.js';
+import {
+  DocumentPathItemMappingError,
+  DocumentPathMappingError,
+} from '../errors/index.js';
 
 export function getNestedMappingSchema(
   mappingSchema: MappingSchema,
@@ -33,4 +39,38 @@ export function mapDocumentPathItem(
     attributeName: mappedAttributeName,
     index,
   };
+}
+
+export function mapDocumentPath(
+  mappingSchema: MappingSchema,
+  documentPath: DocumentPath,
+): DocumentPath {
+  try {
+    const mappedDocumentPath = [];
+    let currentMappingSchema: MappingSchema | undefined = mappingSchema;
+
+    for (const documentPathItem of documentPath) {
+      if (!currentMappingSchema) {
+        throw new DocumentPathMappingError(documentPath);
+      }
+
+      const mappedDocumentPathItem = mapDocumentPathItem(
+        currentMappingSchema,
+        documentPathItem,
+      );
+
+      const nestedMappingSchema = getNestedMappingSchema(
+        currentMappingSchema,
+        documentPathItem,
+      );
+
+      currentMappingSchema = nestedMappingSchema;
+
+      mappedDocumentPath.push(mappedDocumentPathItem);
+    }
+
+    return mappedDocumentPath;
+  } catch (error) {
+    throw new DocumentPathMappingError(documentPath);
+  }
 }
--- test/mapping/document-path-mapping.test.ts
+++ test/mapping/document-path-mapping.test.ts
@@ -2,10 +2,17 @@ import { describe, it, expect } from '@jest/globals';
 import {
   getNestedMappingSchema,
   mapDocumentPathItem,
+  mapDocumentPath,
 } from '../../src/mapping/document-path-mapping.js';
 import { type MappingSchema } from '../../src/mapping/index.js';
-import { type DocumentPathItem } from '../../src/document-path/index.js';
-import { DocumentPathItemMappingError } from '../../src/errors/index.js';
+import {
+  type DocumentPathItem,
+  type DocumentPath,
+} from '../../src/document-path/index.js';
+import {
+  DocumentPathItemMappingError,
+  DocumentPathMappingError,
+} from '../../src/errors/index.js';
 
 describe('Getting nested mapping schema', () => {
   const mappingSchema: MappingSchema = {
@@ -95,5 +102,107 @@ describe('Mapping document path item', () => {
     expect(() => {
       mapDocumentPathItem(mappingSchema, documentPathItem);
     }).toThrow(DocumentPathItemMappingError);
+  });
+});
+
+describe('Mapping document path', () => {
+  const mappingSchema: MappingSchema = {
+    attr0: {
+      mapsTo: 'a0',
+      nestedMappingSchema: {
+        attr1: {
+          mapsTo: 'b1',
+          nestedMappingSchema: {
+            attr2: {
+              mapsTo: 'c2',
+              nestedMappingSchema: {
+                attr3: { mapsTo: 'd3' },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  type TestCase = {
+    testName: string;
+    documentPath: DocumentPath;
+    mappedDocumentPath: DocumentPath;
+  };
+
+  const testCases: Array<TestCase> = [
+    {
+      testName: 'should map partial document path with one item with no index',
+      documentPath: [{ attributeName: 'attr0' }],
+      mappedDocumentPath: [{ attributeName: 'a0' }],
+    },
+    {
+      testName: 'should map partial document path with one item with index',
+      documentPath: [{ attributeName: 'attr0', index: 0 }],
+      mappedDocumentPath: [{ attributeName: 'a0', index: 0 }],
+    },
+    {
+      testName: 'should map full document path with no index',
+      documentPath: [
+        { attributeName: 'attr0' },
+        { attributeName: 'attr1' },
+        { attributeName: 'attr2' },
+        { attributeName: 'attr3' },
+      ],
+      mappedDocumentPath: [
+        { attributeName: 'a0' },
+        { attributeName: 'b1' },
+        { attributeName: 'c2' },
+        { attributeName: 'd3' },
+      ],
+    },
+    {
+      testName: 'should map full document path with index',
+      documentPath: [
+        { attributeName: 'attr0', index: 0 },
+        { attributeName: 'attr1', index: 1 },
+        { attributeName: 'attr2', index: 2 },
+        { attributeName: 'attr3', index: 3 },
+      ],
+      mappedDocumentPath: [
+        { attributeName: 'a0', index: 0 },
+        { attributeName: 'b1', index: 1 },
+        { attributeName: 'c2', index: 2 },
+        { attributeName: 'd3', index: 3 },
+      ],
+    },
+    {
+      testName: 'should map full document path with mixed indices',
+      documentPath: [
+        { attributeName: 'attr0', index: 0 },
+        { attributeName: 'attr1' },
+        { attributeName: 'attr2', index: 2 },
+        { attributeName: 'attr3' },
+      ],
+      mappedDocumentPath: [
+        { attributeName: 'a0', index: 0 },
+        { attributeName: 'b1' },
+        { attributeName: 'c2', index: 2 },
+        { attributeName: 'd3' },
+      ],
+    },
+  ];
+
+  it.each(testCases)('$testName', ({ documentPath, mappedDocumentPath }) => {
+    const actualMappedDocumentPath = mapDocumentPath(
+      mappingSchema,
+      documentPath,
+    );
+
+    expect(actualMappedDocumentPath).toEqual(mappedDocumentPath);
+  });
+
+  it('should throw error if attribute name is not defined in mapping schema', () => {
+    const documentPath: DocumentPath = [{ attributeName: 'attr4' }];
+
+    expect(() => {
+      mapDocumentPath(mappingSchema, documentPath);
+    }).toThrow(DocumentPathMappingError);
   });
 });
