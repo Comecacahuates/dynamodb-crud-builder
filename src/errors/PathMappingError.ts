ca733ddb117644db395604701ea8eda484c72c48
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-12 13:34:57 -0600

    refactor(errors/PathMappingError): format document path

--- src/errors/PathMappingError.ts
+++ src/errors/PathMappingError.ts
@@ -1,7 +1,10 @@
-import { type DocumentPath } from '../types.js';
+import {
+  type DocumentPath,
+  formatDocumentPath,
+} from '../document-path/index.js';
 
 export class PathMappingError extends Error {
-  constructor(path: DocumentPath) {
-    super(`There is no mapping for path "${path.join('.')}"`);
+  constructor(documentPath: DocumentPath) {
+    super(`There is no mapping for path "${formatDocumentPath(documentPath)}"`);
   }
 }
