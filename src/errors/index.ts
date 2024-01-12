d504a632b1f880e41a45aa5e0b1db2a4bf5558be
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-12 16:07:01 -0600

    clean(errors/DocumentPathItemMappingError)

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -3,4 +3,3 @@ export * from './PutItemError.js';
 export * from './DeleteItemError.js';
 export * from './UpdateItemError.js';
 export * from './InvalidAttributeValueError.js';
-export * from './DocumentPathItemMappingError.js';
