b65b47081ceff50961b3d8dc18ad0a20cfb21be8
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-15 17:37:38 -0600

    test(expressions/update-expressions/statements/add-statements)

--- test/expressions/update-expressions/statements/add-statements.test.ts
+++ test/expressions/update-expressions/statements/add-statements.test.ts
@@ -1,16 +1,44 @@
 import { describe, it, expect } from '@jest/globals';
 import { buildAddStatement } from '../../../../src/expressions/update-expressions/statements/add-statements.js';
 
-describe('Building add statement', () => {
-  it('should return add statement', () => {
-    const actualStatement = buildAddStatement([
-      { attributeName: 'a' },
-      { attributeName: 'b' },
-      { attributeName: 'c', index: 1 },
-      { attributeName: 'd' },
-      { attributeName: 'e', index: 2 },
-    ]);
+describe('building add statement', () => {
+  describe('given document path a', () => {
+    describe('when building an add statement', () => {
+      it('should return "#a :a"', () => {
+        const actualStatement = buildAddStatement([{ attributeName: 'a' }]);
 
-    expect(actualStatement).toBe('#a.#b.#c[1].#d.#e[2] :abc1de2');
+        expect(actualStatement).toBe('#a :a');
+      });
+    });
+  });
+
+  describe('given document path a.b[1].c', () => {
+    describe('when building an add statement', () => {
+      it('should return "#a.#b[1].#c :ab1c"', () => {
+        const actualStatement = buildAddStatement([
+          { attributeName: 'a' },
+          { attributeName: 'b', index: 1 },
+          { attributeName: 'c' },
+        ]);
+
+        expect(actualStatement).toBe('#a.#b[1].#c :ab1c');
+      });
+    });
+  });
+
+  describe('given document path a.b[1].c.d[2].e[3]', () => {
+    describe('when building an add statement', () => {
+      it('should return "#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3"', () => {
+        const actualStatement = buildAddStatement([
+          { attributeName: 'a' },
+          { attributeName: 'b', index: 1 },
+          { attributeName: 'c' },
+          { attributeName: 'd', index: 2 },
+          { attributeName: 'e', index: 3 },
+        ]);
+
+        expect(actualStatement).toBe('#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3');
+      });
+    });
   });
 });
