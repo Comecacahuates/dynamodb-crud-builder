54c56e18abd1c301b966aa172372190ddd03cba9
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-16 15:12:45 -0600

    test(expressions/condition-expressions/statements/comparison-statements)

--- test/expressions/condition-expressions/statements/comparison-statements.test.ts
+++ test/expressions/condition-expressions/statements/comparison-statements.test.ts
@@ -1,25 +1,38 @@
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect, beforeEach } from '@jest/globals';
 import { buildAttributeEqualsAttributeStatement } from '../../../../src/expressions/condition-expressions/statements/comparison-statements.js';
 
-describe('Building attribute equals attribute statement', () => {
-  it('should return equals statement', () => {
-    const actualStatement = buildAttributeEqualsAttributeStatement(
-      [
-        { attributeName: 'a' },
-        { attributeName: 'b' },
-        { attributeName: 'c', index: 1 },
-        { attributeName: 'd' },
-        { attributeName: 'e', index: 2 },
-      ],
-      [
-        { attributeName: 'f' },
-        { attributeName: 'g' },
-        { attributeName: 'h', index: 3 },
-        { attributeName: 'i' },
-        { attributeName: 'j', index: 4 },
-      ],
-    );
+describe('building attribute equals attribute statement', () => {
+  describe('given document path a.b[1].c.d[2].e[3] and document path f.g[4].h.i[5].j[6]', () => {
+    const documentPathA = [
+      { attributeName: 'a' },
+      { attributeName: 'b', index: 1 },
+      { attributeName: 'c' },
+      { attributeName: 'd', index: 2 },
+      { attributeName: 'e', index: 3 },
+    ];
+    const documentPathB = [
+      { attributeName: 'f' },
+      { attributeName: 'g', index: 4 },
+      { attributeName: 'h' },
+      { attributeName: 'i', index: 5 },
+      { attributeName: 'j', index: 6 },
+    ];
 
-    expect(actualStatement).toBe('#a.#b.#c[1].#d.#e[2] = #f.#g.#h[3].#i.#j[4]');
+    describe('when building an equals attribute statement', () => {
+      let equalsStatement: string;
+
+      beforeEach(() => {
+        equalsStatement = buildAttributeEqualsAttributeStatement(
+          documentPathA,
+          documentPathB,
+        );
+      });
+
+      it('should return "#a.#b[1].#c.#d[2].#e[3] = #f.#g[4].#h.#i[5].#j[6]"', () => {
+        expect(equalsStatement).toBe(
+          '#a.#b[1].#c.#d[2].#e[3] = #f.#g[4].#h.#i[5].#j[6]',
+        );
+      });
+    });
   });
 });
