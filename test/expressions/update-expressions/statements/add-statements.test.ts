fcf5420676b9bebfc98d1372d052a7825c4b7dad
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-15 23:37:32 -0600

    test(expressions/update-expressions/statements/add-statements)

--- test/expressions/update-expressions/statements/add-statements.test.ts
+++ test/expressions/update-expressions/statements/add-statements.test.ts
@@ -1,43 +1,61 @@
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect, beforeEach } from '@jest/globals';
 import { buildAddStatement } from '../../../../src/expressions/update-expressions/statements/add-statements.js';
 
 describe('building add statement', () => {
   describe('given document path a', () => {
-    describe('when building an add statement', () => {
-      it('should return "#a :a"', () => {
-        const actualStatement = buildAddStatement([{ attributeName: 'a' }]);
+    const documentPath = [{ attributeName: 'a' }];
 
-        expect(actualStatement).toBe('#a :a');
+    describe('when building an add statement', () => {
+      let addStatement: string;
+
+      beforeEach(() => {
+        addStatement = buildAddStatement(documentPath);
+      });
+
+      it('should return "#a :a"', () => {
+        expect(addStatement).toBe('#a :a');
       });
     });
   });
 
   describe('given document path a.b[1].c', () => {
-    describe('when building an add statement', () => {
-      it('should return "#a.#b[1].#c :ab1c"', () => {
-        const actualStatement = buildAddStatement([
-          { attributeName: 'a' },
-          { attributeName: 'b', index: 1 },
-          { attributeName: 'c' },
-        ]);
+    const documentPath = [
+      { attributeName: 'a' },
+      { attributeName: 'b', index: 1 },
+      { attributeName: 'c' },
+    ];
 
-        expect(actualStatement).toBe('#a.#b[1].#c :ab1c');
+    describe('when building an add statement', () => {
+      let addStatement: string;
+
+      beforeEach(() => {
+        addStatement = buildAddStatement(documentPath);
+      });
+
+      it('should return "#a.#b[1].#c :ab1c"', () => {
+        expect(addStatement).toBe('#a.#b[1].#c :ab1c');
       });
     });
   });
 
   describe('given document path a.b[1].c.d[2].e[3]', () => {
-    describe('when building an add statement', () => {
-      it('should return "#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3"', () => {
-        const actualStatement = buildAddStatement([
-          { attributeName: 'a' },
-          { attributeName: 'b', index: 1 },
-          { attributeName: 'c' },
-          { attributeName: 'd', index: 2 },
-          { attributeName: 'e', index: 3 },
-        ]);
+    const documentPath = [
+      { attributeName: 'a' },
+      { attributeName: 'b', index: 1 },
+      { attributeName: 'c' },
+      { attributeName: 'd', index: 2 },
+      { attributeName: 'e', index: 3 },
+    ];
 
-        expect(actualStatement).toBe('#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3');
+    describe('when building an add statement', () => {
+      let addStatement: string;
+
+      beforeEach(() => {
+        addStatement = buildAddStatement(documentPath);
+      });
+
+      it('should return "#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3"', () => {
+        expect(addStatement).toBe('#a.#b[1].#c.#d[2].#e[3] :ab1cd2e3');
       });
     });
   });
