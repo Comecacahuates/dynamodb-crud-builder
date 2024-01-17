6946300e6ade39f0a250e1d66b4069ebbb2c6bd3
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-17 13:55:03 -0600

    feat(expressions/condition-expressions/statements/function-statements): buildAttributeIsOfTypeStatement function

--- src/expressions/condition-expressions/statements/function-statements.ts
+++ src/expressions/condition-expressions/statements/function-statements.ts
@@ -1,3 +1,10 @@
-export function buildAttributeExistsStatement(operand: string) {
-  return `attribute_exists(${operand})`;
+export function buildAttributeExistsStatement(documentPathPlaceholder: string) {
+  return `attribute_exists(${documentPathPlaceholder})`;
+}
+
+export function buildAttributeIsOfTypeStatement(
+  documentPathPlaceholder: string,
+  attributeTypePlaceholder: string,
+) {
+  return `attribute_type(${documentPathPlaceholder}, ${attributeTypePlaceholder})`;
 }
--- test/expressions/condition-expressions/statements/function-statements.test.ts
+++ test/expressions/condition-expressions/statements/function-statements.test.ts
@@ -1,10 +1,13 @@
 import { describe, it, expect, beforeEach } from '@jest/globals';
-import { buildAttributeExistsStatement } from '../../../../src/expressions/condition-expressions/statements/function-statements.js';
+import {
+  buildAttributeExistsStatement,
+  buildAttributeIsOfTypeStatement,
+} from '../../../../src/expressions/condition-expressions/statements/function-statements.js';
 
 describe('function statements', () => {
-  describe('given operand ":operand" and attribute type "S"', () => {
-    const operand = ':operand';
-    const attributeType = 'S';
+  describe('given document path placeholder "#a[1].#b.#c[2]" and attribute type placeholder ":attributeType"', () => {
+    const operand = '#a[1].#b.#c[2]';
+    const attributeType = ':attributeType';
 
     describe('when building `attribute exists` statement', () => {
       let attributeExistsStatement: string;
@@ -13,8 +16,27 @@ describe('function statements', () => {
         attributeExistsStatement = buildAttributeExistsStatement(operand);
       });
 
-      it('should return "attribute_exists(:operand)"', () => {
-        expect(attributeExistsStatement).toBe('attribute_exists(:operand)');
+      it('should return "attribute_exists(#a[1].#b.#c[2])"', () => {
+        expect(attributeExistsStatement).toBe(
+          'attribute_exists(#a[1].#b.#c[2])',
+        );
+      });
+    });
+
+    describe('when building `attribute is of type` statement', () => {
+      let attributeIsOfTypeStatement: string;
+
+      beforeEach(() => {
+        attributeIsOfTypeStatement = buildAttributeIsOfTypeStatement(
+          operand,
+          attributeType,
+        );
+      });
+
+      it('should return "attribute_type(#a[1].#b.#c[2], :attributeType)"', () => {
+        expect(attributeIsOfTypeStatement).toBe(
+          'attribute_type(#a[1].#b.#c[2], :attributeType)',
+        );
       });
     });
   });
