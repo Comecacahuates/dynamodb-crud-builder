12ec324c2c86eea06d269ef78bb20c7fa5e8fc03
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-16 18:11:42 -0600

    feat(expressions/condition-expressions/statements/comparison-statements): buildNotEqualsComparisonStatement function

--- src/expressions/condition-expressions/statements/comparison-statements.ts
+++ src/expressions/condition-expressions/statements/comparison-statements.ts
@@ -1,7 +1,3 @@
-import { buildExpressionAttributeNamePlaceholder } from '../../expression-attribute-names.js';
-import { buildExpressionAttributeValuePlaceholder } from '../../expression-attribute-values.js';
-import { type DocumentPath } from '../../../document-path/types.js';
-
 export function buildEqualsComparisonStatement(
   operandA: string,
   operandB: string,
@@ -9,65 +5,9 @@ export function buildEqualsComparisonStatement(
   return `${operandA} = ${operandB}`;
 }
 
-export function buildAttributeEqualsAttributeStatement(
-  documentPathA: DocumentPath,
-  documentPathB: DocumentPath,
+export function buildNotEqualsComparisonStatement(
+  operandA: string,
+  operandB: string,
 ): string {
-  const expressionAttributeNamePlaceholderA =
-    buildExpressionAttributeNamePlaceholder(documentPathA);
-
-  const expressionAttributeNamePlaceholderB =
-    buildExpressionAttributeNamePlaceholder(documentPathB);
-
-  return `${expressionAttributeNamePlaceholderA} = ${expressionAttributeNamePlaceholderB}`;
-}
-
-export function buildAttributeEqualsLiteralStatement(
-  documentPath: DocumentPath,
-  documentPathForLiteral: DocumentPath,
-): string {
-  const expressionAttributeNamePlaceholder =
-    buildExpressionAttributeNamePlaceholder(documentPath);
-  const expressionAttributeValuePlaceholder =
-    buildExpressionAttributeValuePlaceholder(documentPathForLiteral);
-
-  return `${expressionAttributeNamePlaceholder} = ${expressionAttributeValuePlaceholder}`;
-}
-
-export function buildAttributeNotEqualsAttributeStatement(
-  documentPathA: DocumentPath,
-  documentPathB: DocumentPath,
-): string {
-  const expressionAttributeNamePlaceholderA =
-    buildExpressionAttributeNamePlaceholder(documentPathA);
-
-  const expressionAttributeNamePlaceholderB =
-    buildExpressionAttributeNamePlaceholder(documentPathB);
-
-  return `${expressionAttributeNamePlaceholderA} <> ${expressionAttributeNamePlaceholderB}`;
-}
-
-export function buildAttributeNotEqualsLiteralStatement(
-  documentPath: DocumentPath,
-  documentPathForLiteral: DocumentPath,
-): string {
-  const expressionAttributeNamePlaceholder =
-    buildExpressionAttributeNamePlaceholder(documentPath);
-  const expressionAttributeValuePlaceholder =
-    buildExpressionAttributeValuePlaceholder(documentPathForLiteral);
-
-  return `${expressionAttributeNamePlaceholder} <> ${expressionAttributeValuePlaceholder}`;
-}
-
-export function buildAttributeIsLessThanAttributeStatement(
-  documentPathA: DocumentPath,
-  documentPathB: DocumentPath,
-): string {
-  const expressionAttributeNamePlaceholderA =
-    buildExpressionAttributeNamePlaceholder(documentPathA);
-
-  const expressionAttributeNamePlaceholderB =
-    buildExpressionAttributeNamePlaceholder(documentPathB);
-
-  return `${expressionAttributeNamePlaceholderA} < ${expressionAttributeNamePlaceholderB}`;
+  return `${operandA} <> ${operandB}`;
 }
--- test/expressions/condition-expressions/statements/comparison-statements.test.ts
+++ test/expressions/condition-expressions/statements/comparison-statements.test.ts
@@ -1,11 +1,7 @@
 import { describe, it, expect, beforeEach } from '@jest/globals';
 import {
   buildEqualsComparisonStatement,
-  buildAttributeEqualsAttributeStatement,
-  buildAttributeEqualsLiteralStatement,
-  buildAttributeNotEqualsAttributeStatement,
-  buildAttributeNotEqualsLiteralStatement,
-  buildAttributeIsLessThanAttributeStatement,
+  buildNotEqualsComparisonStatement,
 } from '../../../../src/expressions/condition-expressions/statements/comparison-statements.js';
 
 describe('equals', () => {
@@ -27,173 +23,20 @@ describe('equals', () => {
   });
 });
 
-describe('attribute equals attribute', () => {
-  describe('given document path a.b[1].c.d[2].e[3] and document path f.g[4].h.i[5].j[6]', () => {
-    const documentPathA = [
-      { attributeName: 'a' },
-      { attributeName: 'b', index: 1 },
-      { attributeName: 'c' },
-      { attributeName: 'd', index: 2 },
-      { attributeName: 'e', index: 3 },
-    ];
-    const documentPathB = [
-      { attributeName: 'f' },
-      { attributeName: 'g', index: 4 },
-      { attributeName: 'h' },
-      { attributeName: 'i', index: 5 },
-      { attributeName: 'j', index: 6 },
-    ];
+describe('not equals', () => {
+  describe('given operand A ":operandA" and operand B ":operandB"', () => {
+    const operandA = ':operandA';
+    const operandB = ':operandB';
 
     describe('when building the equals statement', () => {
       let equalsStatement: string;
 
       beforeEach(() => {
-        equalsStatement = buildAttributeEqualsAttributeStatement(
-          documentPathA,
-          documentPathB,
-        );
+        equalsStatement = buildNotEqualsComparisonStatement(operandA, operandB);
       });
 
-      it('should return "#a.#b[1].#c.#d[2].#e[3] = #f.#g[4].#h.#i[5].#j[6]"', () => {
-        expect(equalsStatement).toBe(
-          '#a.#b[1].#c.#d[2].#e[3] = #f.#g[4].#h.#i[5].#j[6]',
-        );
-      });
-    });
-  });
-});
-
-describe('attribute equals literal', () => {
-  describe('given document path a.b[1].c.d[2].e[3] and literal value 1 with document path DocumentPathForLiteral', () => {
-    const documentPath = [
-      { attributeName: 'a' },
-      { attributeName: 'b', index: 1 },
-      { attributeName: 'c' },
-      { attributeName: 'd', index: 2 },
-      { attributeName: 'e', index: 3 },
-    ];
-    const documentPathForLiteral = [
-      { attributeName: 'DocumentPathForLiteral' },
-    ];
-
-    describe('when building the equals statement', () => {
-      let equalsStatement: string;
-
-      beforeEach(() => {
-        equalsStatement = buildAttributeEqualsLiteralStatement(
-          documentPath,
-          documentPathForLiteral,
-        );
-      });
-
-      it('should return "#a.#b[1].#c.#d[2].#e[3] = :DocumentPathForLiteral"', () => {
-        expect(equalsStatement).toBe(
-          '#a.#b[1].#c.#d[2].#e[3] = :DocumentPathForLiteral',
-        );
-      });
-    });
-  });
-});
-
-describe('attribute not equals attribute', () => {
-  describe('given document path a.b[1].c.d[2].e[3] and document path f.g[4].h.i[5].j[6]', () => {
-    const documentPathA = [
-      { attributeName: 'a' },
-      { attributeName: 'b', index: 1 },
-      { attributeName: 'c' },
-      { attributeName: 'd', index: 2 },
-      { attributeName: 'e', index: 3 },
-    ];
-    const documentPathB = [
-      { attributeName: 'f' },
-      { attributeName: 'g', index: 4 },
-      { attributeName: 'h' },
-      { attributeName: 'i', index: 5 },
-      { attributeName: 'j', index: 6 },
-    ];
-
-    describe('when building the equals statement', () => {
-      let equalsStatement: string;
-
-      beforeEach(() => {
-        equalsStatement = buildAttributeNotEqualsAttributeStatement(
-          documentPathA,
-          documentPathB,
-        );
-      });
-
-      it('should return "#a.#b[1].#c.#d[2].#e[3] <> #f.#g[4].#h.#i[5].#j[6]"', () => {
-        expect(equalsStatement).toBe(
-          '#a.#b[1].#c.#d[2].#e[3] <> #f.#g[4].#h.#i[5].#j[6]',
-        );
-      });
-    });
-  });
-});
-
-describe('attribute not equals literal', () => {
-  describe('given document path a.b[1].c.d[2].e[3] and literal value 1 with document path DocumentPathForLiteral', () => {
-    const documentPath = [
-      { attributeName: 'a' },
-      { attributeName: 'b', index: 1 },
-      { attributeName: 'c' },
-      { attributeName: 'd', index: 2 },
-      { attributeName: 'e', index: 3 },
-    ];
-    const documentPathForLiteral = [
-      { attributeName: 'DocumentPathForLiteral' },
-    ];
-
-    describe('when building the equals statement', () => {
-      let equalsStatement: string;
-
-      beforeEach(() => {
-        equalsStatement = buildAttributeNotEqualsLiteralStatement(
-          documentPath,
-          documentPathForLiteral,
-        );
-      });
-
-      it('should return "#a.#b[1].#c.#d[2].#e[3] <> :DocumentPathForLiteral"', () => {
-        expect(equalsStatement).toBe(
-          '#a.#b[1].#c.#d[2].#e[3] <> :DocumentPathForLiteral',
-        );
-      });
-    });
-  });
-});
-
-describe('attribute is less than attribute', () => {
-  describe('given document path a.b[1].c.d[2].e[3] and document path f.g[4].h.i[5].j[6]', () => {
-    const documentPathA = [
-      { attributeName: 'a' },
-      { attributeName: 'b', index: 1 },
-      { attributeName: 'c' },
-      { attributeName: 'd', index: 2 },
-      { attributeName: 'e', index: 3 },
-    ];
-    const documentPathB = [
-      { attributeName: 'f' },
-      { attributeName: 'g', index: 4 },
-      { attributeName: 'h' },
-      { attributeName: 'i', index: 5 },
-      { attributeName: 'j', index: 6 },
-    ];
-
-    describe('when building the equals statement', () => {
-      let equalsStatement: string;
-
-      beforeEach(() => {
-        equalsStatement = buildAttributeIsLessThanAttributeStatement(
-          documentPathA,
-          documentPathB,
-        );
-      });
-
-      it('should return "#a.#b[1].#c.#d[2].#e[3] < #f.#g[4].#h.#i[5].#j[6]"', () => {
-        expect(equalsStatement).toBe(
-          '#a.#b[1].#c.#d[2].#e[3] < #f.#g[4].#h.#i[5].#j[6]',
-        );
+      it('should return ":operandA <> :operandB"', () => {
+        expect(equalsStatement).toBe(':operandA <> :operandB');
       });
     });
   });
