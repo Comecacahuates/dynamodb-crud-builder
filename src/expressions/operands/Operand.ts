15e4f1006e6d8cd1c19e3a21bb3a079980b31e95
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-19 12:35:59 -0600

    feat(expressions/operands/Operand): add expression attribute names and expression attribute values

--- src/expressions/operands/Operand.ts
+++ src/expressions/operands/Operand.ts
@@ -1,11 +1,49 @@
 import { Condition } from '../conditions/Condition.js';
+import {
+  type ExpressionAttributeNames,
+  type ExpressionAttributeValues,
+} from '../types.js';
 
 export class Operand {
-  public constructor(public readonly symbolicValue: string) {}
+  public constructor(
+    public readonly symbolicValue: string,
+    public readonly expressionAttributeNames: ExpressionAttributeNames = {},
+    public readonly expressionAttributeValues: ExpressionAttributeValues = {},
+  ) {}
+
+  private static mergeExpressionAttributeNames(operands: Array<Operand>) {
+    return operands.reduce(
+      (expressionAttributeNames, condition) => ({
+        ...expressionAttributeNames,
+        ...condition.expressionAttributeNames,
+      }),
+      {},
+    );
+  }
+
+  private static mergeExpressionAttributeValues(operands: Array<Operand>) {
+    return operands.reduce(
+      (expressionAttributeValues, condition) => ({
+        ...expressionAttributeValues,
+        ...condition.expressionAttributeValues,
+      }),
+      {},
+    );
+  }
 
   public equals(otherOperand: Operand): Condition {
+    const allOperands = [this, otherOperand];
+
+    const expressionAttributeNames =
+      Operand.mergeExpressionAttributeNames(allOperands);
+
+    const expressionAttributeValues =
+      Operand.mergeExpressionAttributeValues(allOperands);
+
     return new Condition(
       `${this.symbolicValue} = ${otherOperand.symbolicValue}`,
+      expressionAttributeNames,
+      expressionAttributeValues,
     );
   }
 }
--- test/expressions/operands/Operand/Operand.comparisons.test.ts
+++ test/expressions/operands/Operand/Operand.comparisons.test.ts
@@ -1,17 +1,49 @@
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect, beforeEach } from '@jest/globals';
 import { Operand } from '../../../../src/expressions/operands/Operand.js';
 import { Condition } from '../../../../src/expressions/conditions/Condition.js';
 
 describe('comparisons', () => {
   describe('given operands opA and opB', () => {
-    const opA = new Operand('opA');
-    const opB = new Operand('opB');
+    const opA = new Operand(
+      '#opA.#attr0[1][2].#attr1',
+      {
+        '#opA': 'opA',
+        '#attr0': 'attr0',
+        '#attr1': 'attr1',
+      },
+      {},
+    );
+    const opB = new Operand(':opB', {}, { ':opB': { N: '1' } });
 
     describe('when comparing equality', () => {
-      const comparison = opA.equals(opB);
+      let comparison: Condition;
+
+      beforeEach(() => {
+        comparison = opA.equals(opB);
+      });
 
       it('should return a condition', () => {
         expect(comparison).toBeInstanceOf(Condition);
+      });
+
+      it('should return a condition with symbolic value :opA = :opB', () => {
+        expect(comparison.symbolicValue).toBe(
+          '#opA.#attr0[1][2].#attr1 = :opB',
+        );
+      });
+
+      it('should return a condition with expression attribute names', () => {
+        expect(comparison.expressionAttributeNames).toEqual({
+          '#opA': 'opA',
+          '#attr0': 'attr0',
+          '#attr1': 'attr1',
+        });
+      });
+
+      it('should return a condition with expression attribute values', () => {
+        expect(comparison.expressionAttributeValues).toEqual({
+          ':opB': { N: '1' },
+        });
       });
     });
   });
