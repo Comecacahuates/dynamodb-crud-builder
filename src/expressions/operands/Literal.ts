028203543d64062e8f0983e94feacd86680226cb
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-19 12:59:37 -0600

    clean(expressions/operands/Literal): remove unnecessary methods

--- src/expressions/operands/Literal.ts
+++ src/expressions/operands/Literal.ts
@@ -1,11 +1,3 @@
 import { type AttributeValue } from '@aws-sdk/client-dynamodb';
 
-export class Literal {
-  public static parseNull(attributeValue: AttributeValue.NULLMember): null {
-    return null;
-  }
-
-  public static parseString(attributeValue: AttributeValue.SMember): string {
-    return attributeValue.S;
-  }
-}
+export class Literal {}
--- test/expressions/operands/Literal/Literal.ts
+++ test/expressions/operands/Literal/Literal.ts
@@ -1,37 +1,3 @@
 import { describe, it, expect, beforeEach } from '@jest/globals';
 import { type AttributeValue } from '@aws-sdk/client-dynamodb';
 import { Literal } from '../../../../src/expressions/operands/Literal.js';
-
-describe('parsing', () => {
-  describe('given null attribute value', () => {
-    const attributeValue: AttributeValue.NULLMember = { NULL: true };
-
-    describe('when parsing null', () => {
-      let parsed: null;
-
-      beforeEach(() => {
-        parsed = Literal.parseNull(attributeValue);
-      });
-
-      it('should return null value', () => {
-        expect(parsed).toBeNull();
-      });
-    });
-  });
-
-  describe('given string attribute value', () => {
-    const attributeValue: AttributeValue.SMember = { S: 'value' };
-
-    describe('when parsing string', () => {
-      let parsed: string;
-
-      beforeEach(() => {
-        parsed = Literal.parseString(attributeValue);
-      });
-
-      it('should return string value', () => {
-        expect(parsed).toBe('value');
-      });
-    });
-  });
-});
