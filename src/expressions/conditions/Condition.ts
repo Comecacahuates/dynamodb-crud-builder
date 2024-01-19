8bd8b3d4f32dfcd89885e82cd43afa34d949792a
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-19 12:17:09 -0600

    refactor(expressions/conditions/Condition): add method

--- src/expressions/conditions/Condition.ts
+++ src/expressions/conditions/Condition.ts
@@ -10,30 +10,40 @@ export class Condition {
     public readonly expressionAttributeValues: ExpressionAttributeValues = {},
   ) {}
 
-  public and(...conditions: Array<Condition>): Condition {
-    const allConditions = [this, ...conditions];
-    const conjunction = allConditions
-      .map((condition) => condition.symbolicValue)
-      .join(' AND ');
-
-    const expressionAttributeNames = allConditions.reduce(
+  private static mergeExpressionAttributeNames(conditions: Array<Condition>) {
+    return conditions.reduce(
       (expressionAttributeNames, condition) => ({
         ...expressionAttributeNames,
         ...condition.expressionAttributeNames,
       }),
       {},
     );
+  }
 
-    const expressionAttributeValues = allConditions.reduce(
+  private static mergeExpressionAttributeValues(conditions: Array<Condition>) {
+    return conditions.reduce(
       (expressionAttributeValues, condition) => ({
         ...expressionAttributeValues,
         ...condition.expressionAttributeValues,
       }),
       {},
     );
+  }
+
+  public and(...conditions: Array<Condition>): Condition {
+    const allConditions = [this, ...conditions];
+    const conjunctionSymbolicValue = allConditions
+      .map((condition) => condition.symbolicValue)
+      .join(' AND ');
+
+    const expressionAttributeNames =
+      Condition.mergeExpressionAttributeNames(allConditions);
+
+    const expressionAttributeValues =
+      Condition.mergeExpressionAttributeValues(allConditions);
 
     return new Condition(
-      `(${conjunction})`,
+      `(${conjunctionSymbolicValue})`,
       expressionAttributeNames,
       expressionAttributeValues,
     );
