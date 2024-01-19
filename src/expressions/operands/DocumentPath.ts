d2556f66ede9e80a99b3a11fdf24419b8e894588
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-19 15:06:54 -0600

    refactor(expressions/operands/DocumentPath): buildExpressionAttributeNames method

--- src/expressions/operands/DocumentPath.ts
+++ src/expressions/operands/DocumentPath.ts
@@ -3,30 +3,33 @@ import { DocumentPathItem } from './DocumentPathItem.js';
 import { ExpressionAttributeNames } from '../../expressions/index.js';
 
 export class DocumentPath extends Operand {
-  public constructor(public readonly items: Array<DocumentPathItem>) {
-    const symbolicValue = DocumentPath.buildSymbolicValue(items);
+  public constructor(
+    public readonly documentPathItems: Array<DocumentPathItem>,
+  ) {
+    const symbolicValue = DocumentPath.buildSymbolicValue(documentPathItems);
     const expressionAttributeNames =
-      DocumentPath.buildExpressionAttributeNames(items);
+      DocumentPath.buildExpressionAttributeNames(documentPathItems);
     const expressionAttributeValues = undefined;
 
     super(symbolicValue, expressionAttributeNames, expressionAttributeValues);
   }
 
-  private static buildSymbolicValue(items: Array<DocumentPathItem>): string {
-    return items.map((item) => `#${item.toString()}`).join('.');
+  private static buildSymbolicValue(
+    documentPathItems: Array<DocumentPathItem>,
+  ): string {
+    return documentPathItems.map((item) => `#${item.toString()}`).join('.');
   }
 
   private static buildExpressionAttributeNames(
-    items: Array<DocumentPathItem>,
+    documentPathItems: Array<DocumentPathItem>,
   ): ExpressionAttributeNames {
-    const expressionAttributeNames: ExpressionAttributeNames = {};
-
-    for (const documentPathItem of items) {
-      expressionAttributeNames[`#${documentPathItem.attributeName}`] =
-        documentPathItem.attributeName;
-    }
-
-    return expressionAttributeNames;
+    return documentPathItems.reduce(
+      (expressionAttributeNames, documentPathItem) => ({
+        ...expressionAttributeNames,
+        [`#${documentPathItem.attributeName}`]: documentPathItem.attributeName,
+      }),
+      {},
+    );
   }
 
   public static parse(documentPathString: string): DocumentPath | null {
@@ -45,21 +48,6 @@ export class DocumentPath extends Operand {
   }
 
   public override toString(): string {
-    return this.items.map((item) => item.toString()).join('.');
-  }
-
-  public getExpressionAttributeNamesPlaceholder(): string {
-    return this.items.map((item) => `#${item.toString()}`).join('.');
-  }
-
-  public getExpressionAttributeNames(): ExpressionAttributeNames {
-    const expressionAttributeNames: ExpressionAttributeNames = {};
-
-    for (const documentPathItem of this.items) {
-      expressionAttributeNames[`#${documentPathItem.attributeName}`] =
-        documentPathItem.attributeName;
-    }
-
-    return expressionAttributeNames;
+    return this.documentPathItems.map((item) => item.toString()).join('.');
   }
 }
