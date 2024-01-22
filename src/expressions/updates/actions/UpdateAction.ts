e4ded3c893f275e3bfe5c29422b836d5d30f2e02
Adrián Juárez <comecacahuates@yahoo.com>
2024-01-22 08:52:23 -0600

    feat(expressions/updates/actions/UpdateAction): setValue method

--- src/expressions/updates/actions/UpdateAction.ts
+++ src/expressions/updates/actions/UpdateAction.ts
@@ -1,16 +1,42 @@
+import clone from 'just-clone';
+import { type DocumentPath } from '../../operands/DocumentPath.js';
+import { type Operand } from '../../operands/Operand.js';
 import {
   type ExpressionAttributeNames,
   type ExpressionAttributeValues,
 } from '../../types.js';
-import clone from 'just-clone';
+import { mergeExpressionAttributeNames } from '../../expression-attribute-names.js';
+import { mergeExpressionAttributeValues } from '../../expression-attribute-values.js';
 
-export abstract class UpdateAction {
+export enum UpdateActionType {
+  SET,
+  REMOVE,
+  ADD,
+  DELETE,
+}
+
+export class UpdateAction {
   public constructor(
+    public readonly type: UpdateActionType,
     public readonly statement: string,
     public readonly expressionAttributeNames: ExpressionAttributeNames,
     public readonly expressionAttributeValues: ExpressionAttributeValues,
   ) {
     this.expressionAttributeNames = clone(expressionAttributeNames);
     this.expressionAttributeValues = clone(expressionAttributeValues);
+  }
+
+  public static setValue(
+    documentPath: DocumentPath,
+    value: Operand,
+  ): UpdateAction {
+    const statement = `${documentPath.symbolicValue} = ${value.symbolicValue}`;
+
+    return new UpdateAction(
+      UpdateActionType.SET,
+      statement,
+      mergeExpressionAttributeNames([documentPath, value]),
+      mergeExpressionAttributeValues([documentPath, value]),
+    );
   }
 }
---
+++ test/expressions/updates/actions/UpdateAction.set.test.ts
@@ -0,0 +1,74 @@
+import { describe, it, expect } from '@jest/globals';
+import {
+  UpdateAction,
+  UpdateActionType,
+} from '../../../../src/expressions/updates/actions/UpdateAction.js';
+import { DocumentPath } from '../../../../src/expressions/operands/DocumentPath.js';
+import { Literal } from '../../../../src/expressions/operands/Literal.js';
+
+describe('creating action to set value', () => {
+  describe('given document path "a[0].b.c[1][2]" and literal number 1 named "Number"', () => {
+    const documentPath = DocumentPath.parse('a[0].b.c[1][2]');
+    const literal = Literal.fromValue(1, 'Number');
+
+    describe('when creating a set action', () => {
+      const setAction = UpdateAction.setValue(documentPath, literal);
+
+      it('should have the type "SET"', () => {
+        expect(setAction.type).toBe(UpdateActionType.SET);
+      });
+
+      it('should have the statement "#a[0].#b.#c[1][2] = :literalNumber"', () => {
+        expect(setAction.statement).toBe('#a[0].#b.#c[1][2] = :literalNumber');
+      });
+
+      it('should have the expression attribute names of the document path', () => {
+        expect(setAction.expressionAttributeNames).toEqual({
+          '#a': 'a',
+          '#b': 'b',
+          '#c': 'c',
+        });
+      });
+
+      it('should have the expression attribute values of the literal', () => {
+        expect(setAction.expressionAttributeValues).toEqual({
+          ':literalNumber': { N: '1' },
+        });
+      });
+    });
+  });
+
+  describe('given document path A "a[0].b.c[1][2]" and document path B "d[0].e.f[1][3]"', () => {
+    const documentPathA = DocumentPath.parse('a[0].b.c[1][2]');
+    const documentPathB = DocumentPath.parse('d[0].e.f[1][3]');
+
+    describe('when creating a set action', () => {
+      const setAction = UpdateAction.setValue(documentPathA, documentPathB);
+
+      it('should have the type "SET"', () => {
+        expect(setAction.type).toBe(UpdateActionType.SET);
+      });
+
+      it('should have the statement "#a[0].#b.#c[1][2] = #d[0].#e.#f[1][3]"', () => {
+        expect(setAction.statement).toBe(
+          '#a[0].#b.#c[1][2] = #d[0].#e.#f[1][3]',
+        );
+      });
+
+      it('should have the expression attribute names of document paths', () => {
+        expect(setAction.expressionAttributeNames).toEqual({
+          '#a': 'a',
+          '#b': 'b',
+          '#c': 'c',
+          '#d': 'd',
+          '#e': 'e',
+          '#f': 'f',
+        });
+      });
+
+      it('should have no expression attribute values', () => {
+        expect(setAction.expressionAttributeValues).toEqual({});
+      });
+    });
+  });
+});
