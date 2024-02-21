f7c97362659813835f0772c3eb634816104262a1
Adrián Juárez <comecacahuates@yahoo.com>
2024-02-21 16:51:39 -0600

    clean(types): remove AttributeType

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,3 @@
-export type AttributeType =
-  | string
-  | number
-  | null
-  | boolean
-  | Uint8Array
-  | Set<string>
-  | Set<number>
-  | Set<Uint8Array>
-  | AttributeType[]
-  | { [key: string]: AttributeType };
-
 export type ValueUpdateOptions = {
   preventOverwriting?: boolean;
 };
