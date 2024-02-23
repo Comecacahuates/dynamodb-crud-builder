54cec256f9f30f907ef894b45120b2e3522e05eb
Adrián Juárez <comecacahuates@yahoo.com>
2024-02-22 20:36:51 -0600

    clean(expressions/update-expressions/statements/remove-statements)

--- src/expressions/update-expressions/statements/index.ts
+++ src/expressions/update-expressions/statements/index.ts
@@ -1,2 +1,1 @@
 export * as Set from './set-statements.js';
-export * as Remove from './remove-statements.js';
--- src/expressions/update-expressions/statements/remove-statements.ts
+++
@@ -1,9 +0,0 @@
-import { buildExpressionAttributeNamePlaceholder } from '../../expression-attribute-names.js';
-import { type DocumentPath } from '../../../document-path/index.js';
-
-export function buildStatementToRemove(documentPath: DocumentPath): string {
-  const expressionAttributeNamePlaceholder =
-    buildExpressionAttributeNamePlaceholder(documentPath);
-
-  return `${expressionAttributeNamePlaceholder}`;
-}
