890cd4c73097a63455f748aebab064e6905178f8
Adrián Juárez <comecacahuates@yahoo.com>
2024-02-23 17:36:27 -0600

    clean(mapping/item-mapping)

--- src/mapping/index.ts
+++ src/mapping/index.ts
@@ -1,4 +1,4 @@
 export * from './types.js';
 export * from './reverse-item-mapping.js';
-export * from './item-mapping.js';
 export * from './document-path-mapping.js';
+export * from './AttributeNameMapper.js';
