300b18eba2e40d4386643099b44a5292dc98ff38
Adrián Juárez <comecacahuates@yahoo.com>
2024-02-22 20:50:50 -0600

    clean(expressions/expression-attribute-values)

--- src/expressions/expression-attribute-values.ts
+++
@@ -1,7 +0,0 @@
-import { type DocumentPath } from '../document-path/index.js';
-
-export function buildExpressionAttributeValuePlaceholder(
-  documentPath: DocumentPath,
-): string {
-  return `:${documentPath.map(Object.values).flat().join('')}`;
-}
