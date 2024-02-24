bf4ec076c9677d422de296f930b25fc7ffbf21a0
Adrián Juárez <comecacahuates@yahoo.com>
2024-02-24 00:01:50 -0600

    feat(types)

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
-export type ValueUpdateOptions = {
-  preventOverwriting?: boolean;
-};
+import { type NativeAttributeValue } from '@aws-sdk/util-dynamodb';
+
+export type AttributeName = string;
+export type NativeObjectAttributeValue = Record<
+  AttributeName,
+  NativeAttributeValue
+>;
+export type NativeArrayAttributeValue = Array<NativeAttributeValue>;
+export type NativeTupleAttributeValue = Array<NativeAttributeValue>;
+export type DatabaseItem = Record<
+  AttributeName,
+  NativeAttributeValue | NativeObjectAttributeValue | NativeArrayAttributeValue
+>;
+export type DatabaseItemAttribute = [AttributeName, NativeAttributeValue];
+export type DatabaseItemAttributes = Array<DatabaseItemAttribute>;
