945fe8f08fbd189b86059a5e4540689d555445d2
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-17 14:01:53 -0600

    chore(errpors): update exports

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,6 +1,2 @@
-export * from './PutItemError.js';
-export * from './DeleteItemError.js';
-export * from './UpdateItemError.js';
 export * from './InvalidAttributeValueError.js';
-export * from './ItemMappingError.js';
 export * from './DocumentPathParsingError.js';
