a6a71e63786942348e828ff25b1f6f4f0d24220f
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-16 21:21:49 -0600

    clean(errors): remove unnecessary errors

--- src/errors/ItemMappingError.ts
+++
@@ -1,9 +0,0 @@
-import { type Item } from '../types.js';
-
-export class ItemMappingError extends Error {
-  public override readonly name = 'ItemMappingError';
-
-  constructor(item: Item) {
-    super(`Failed to map item "${JSON.stringify(item)}"`);
-  }
-}
