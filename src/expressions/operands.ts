c8967e4fb19f55d3d17f67d812bed2384970761d
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-16 21:13:49 -0600

    clean(expressions/types)

--- src/expressions/operands.ts
+++
@@ -1,5 +0,0 @@
-import { type Operand } from './types.js';
-
-export function isOperandLiteral(operand: Operand): boolean {
-  return typeof operand === 'string';
-}
--- src/expressions/types.ts
+++
@@ -1,5 +0,0 @@
-import { type DocumentPath } from '../document-path/types.js';
-
-export type Literal = string;
-
-export type Operand = DocumentPath | Literal;
