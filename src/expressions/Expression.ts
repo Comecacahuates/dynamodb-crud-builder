aa51b072e2c5cffb0259d5cc02adb630a5b335f3
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-16 19:27:11 -0600

    refactor(expressions/Expression): use AttributeNames and AttributeValues instead of ExpressionAttributeNames and ExpressionAttributeValues

--- src/expressions/Expression.ts
+++ src/expressions/Expression.ts
@@ -1,8 +1,10 @@
-import { type ExpressionAttributeNames } from './ExpressionAttributeNames.js';
-import { type ExpressionAttributeValues } from './ExpressionAttributeValues.js';
+import {
+  type AttributeNames,
+  type AttributeValues,
+} from './attributes/index.js';
 
 export interface Expression {
   getString(): string;
-  getAttributeNames(): ExpressionAttributeNames;
-  getAttributeValues(): ExpressionAttributeValues;
+  getAttributeNames(): AttributeNames;
+  getAttributeValues(): AttributeValues;
 }
