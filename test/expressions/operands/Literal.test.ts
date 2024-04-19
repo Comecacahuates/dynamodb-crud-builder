af8879c3e5b2c7d3c47caed558ad7a0ede983df9
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-19 10:12:14 -0600

    test(expressions/operands/Literal): test more literal types

--- test/expressions/operands/Literal.test.ts
+++ test/expressions/operands/Literal.test.ts
@@ -12,7 +12,7 @@ describe('creating from value', () => {
         expect(literal.getString()).toMatch(/:literal\w{10}/);
       });
 
-      it('should have no attribute names', () => {
+      it('should have empty attribute names', () => {
         expect(
           literal.getAttributeNames().toExpressionAttributeNames(),
         ).toEqual({});
@@ -29,6 +29,68 @@ describe('creating from value', () => {
         expect(keys[0]).toMatch(/:literal\w{10}/);
         expect(values).toHaveLength(1);
         expect(values[0]).toEqual({ S: 'value' });
+      });
+    });
+  });
+
+  describe('given a number value', () => {
+    const value = 123;
+
+    describe('when creating a literal', () => {
+      const literal = new Literal(value);
+
+      it('should have a expression string', () => {
+        expect(literal.getString()).toMatch(/:literal\w{10}/);
+      });
+
+      it('should have empty attribute names', () => {
+        expect(
+          literal.getAttributeNames().toExpressionAttributeNames(),
+        ).toEqual({});
+      });
+
+      it('should have attribute values', () => {
+        const expressionAttributeValues = literal
+            .getAttributeValues()
+            .toExpressionAttributeValues(),
+          keys = Object.keys(expressionAttributeValues),
+          values = Object.values(expressionAttributeValues);
+
+        expect(keys).toHaveLength(1);
+        expect(keys[0]).toMatch(/:literal\w{10}/);
+        expect(values).toHaveLength(1);
+        expect(values[0]).toEqual({ N: '123' });
+      });
+    });
+  });
+
+  describe('given a boolean value', () => {
+    const value = true;
+
+    describe('when creating a literal', () => {
+      const literal = new Literal(value);
+
+      it('should have a expression string', () => {
+        expect(literal.getString()).toMatch(/:literal\w{10}/);
+      });
+
+      it('should have empty attribute names', () => {
+        expect(
+          literal.getAttributeNames().toExpressionAttributeNames(),
+        ).toEqual({});
+      });
+
+      it('should have attribute values', () => {
+        const expressionAttributeValues = literal
+            .getAttributeValues()
+            .toExpressionAttributeValues(),
+          keys = Object.keys(expressionAttributeValues),
+          values = Object.values(expressionAttributeValues);
+
+        expect(keys).toHaveLength(1);
+        expect(keys[0]).toMatch(/:literal\w{10}/);
+        expect(values).toHaveLength(1);
+        expect(values[0]).toEqual({ BOOL: true });
       });
     });
   });
