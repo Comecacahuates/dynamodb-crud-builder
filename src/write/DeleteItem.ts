0b207a7c28e1208362542901179fcea7a2b6dc67
Adrián Juárez <comecacahuates@yahoo.com>
2024-04-19 21:30:03 -0600

    feat(write/DeleteItem): add condition expression

--- src/write/DeleteItem.ts
+++ src/write/DeleteItem.ts
@@ -7,19 +7,38 @@ import {
   type DynamoDBClient,
 } from '@aws-sdk/client-dynamodb';
 import { NativeAttributeValue, marshall } from '@aws-sdk/util-dynamodb';
-
+import {
+  AttributeNames,
+  AttributeValues,
+} from '../expressions/attributes/index.js';
+import {
+  type Expression,
+  type ConditionExpression,
+} from '../expressions/index.js';
 export class DeleteItem {
   private deleteItemInput: DeleteItemInput;
+  private attributeNames = new AttributeNames();
+  private attributeValues = new AttributeValues();
 
   public constructor(key: Record<string, NativeAttributeValue>) {
     this.deleteItemInput = {
       Key: marshall(key),
       TableName: undefined,
+      ConditionExpression: undefined,
     };
   }
 
   public fromTable(tableName: string): DeleteItem {
     this.deleteItemInput.TableName = tableName;
+    return this;
+  }
+
+  public onlyIf(conditionExpression: ConditionExpression): DeleteItem {
+    this.deleteItemInput.ConditionExpression = conditionExpression.getString();
+
+    this.mergeAttributeNames(conditionExpression);
+    this.mergeAttributeValues(conditionExpression);
+
     return this;
   }
 
@@ -41,5 +60,17 @@ export class DeleteItem {
     dynamodbClient: DynamoDBClient,
   ): Promise<DeleteItemOutput> {
     return await dynamodbClient.send(this.asCommand());
+  }
+
+  private mergeAttributeNames(expression: Expression): void {
+    this.attributeNames.merge(expression.getAttributeNames());
+    this.deleteItemInput.ExpressionAttributeNames =
+      this.attributeNames.toExpressionAttributeNames();
+  }
+
+  private mergeAttributeValues(expression: Expression): void {
+    this.attributeValues.merge(expression.getAttributeValues());
+    this.deleteItemInput.ExpressionAttributeValues =
+      this.attributeValues.toExpressionAttributeValues();
   }
 }
--- test/write/DeleteItem.test.ts
+++ test/write/DeleteItem.test.ts
@@ -1,13 +1,19 @@
 import { describe, it, expect } from '@jest/globals';
 import { DeleteItem } from '../../src/write/DeleteItem.js';
+import { DocumentPath } from '../../src/expressions/operands/index.js';
 
 describe('delete item', () => {
   describe('given a key and a table name', () => {
     const key = { id: 'id-00' },
-      tableName = 'table-name';
+      tableName = 'table-name',
+      attr0 = new DocumentPath('attr0'),
+      conditionExpression = attr0.notExists();
 
     describe('when building delete item command', () => {
-      const command = new DeleteItem(key).fromTable(tableName).asCommand();
+      const command = new DeleteItem(key)
+        .fromTable(tableName)
+        .onlyIf(conditionExpression)
+        .asCommand();
 
       it('should have key', () => {
         expect(command.input.Key).toEqual({
@@ -17,6 +23,12 @@ describe('delete item', () => {
 
       it('should have table name', () => {
         expect(command.input.TableName).toBe('table-name');
+      });
+
+      it('should have condition expression', () => {
+        expect(command.input.ConditionExpression).toBe(
+          'attribute_not_exists(#attr0)',
+        );
       });
     });
   });
